3faf3c3590ecc88eb94ed80e700ed174ea3b93be
Robbie Cooper <cooperra@users.noreply.github.com>
2024-07-29 18:18:34 -0400

    Whitespace cleanup

--- addons/oip_ui/Plugin.gd
+++ addons/oip_ui/Plugin.gd
@@ -47,69 +47,69 @@ var callable: Callable
 
 func build() -> void:
 	callable.call()
-	
+
 func _process(delta):
 	var root = get_tree().edited_scene_root
-	
+
 	if(root != null && root.has_signal("SimulationStarted")):
 		_run_bar._enable_buttons()
 	else:
 		_run_bar._disable_buttons()
-	
+
 func _enter_tree() -> void:
 	_menu_bar = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(0).get_child(0)
 	_project_popup_menu = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(0).get_child(0).get_child(1)
 	_editor_popup_menu = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(0).get_child(0).get_child(3)
 	_help_popup_menu = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(0).get_child(0).get_child(4)
-	
+
 	_title_bar = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(0)
 	_center_buttons = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(0).get_child(2)
 	_editor_run_bar_container = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(0).get_child(4)
 	_renderer_selection = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(0).get_child(5)
-	
+
 	_separator = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(1).get_child(1).get_child(1).get_child(0).get_child(0).get_child(0).get_child(0).get_child(1).get_child(0).get_child(1).get_child(0).get_child(0).get_child(0).get_child(14)
 	_camera_button = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(1).get_child(1).get_child(1).get_child(0).get_child(0).get_child(0).get_child(0).get_child(1).get_child(0).get_child(1).get_child(0).get_child(0).get_child(0).get_child(15)
-	
+
 	_debugger_button = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(1).get_child(1).get_child(1).get_child(0).get_child(0).get_child(1).get_child(0).get_child(15).get_child(0).get_child(1)
 	_audio_button = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(1).get_child(1).get_child(1).get_child(0).get_child(0).get_child(1).get_child(0).get_child(15).get_child(0).get_child(3)
 	_animation_button = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(1).get_child(1).get_child(1).get_child(0).get_child(0).get_child(1).get_child(0).get_child(15).get_child(0).get_child(4)
-	
+
 	_create_root_vbox = get_tree().root.get_child(0).find_children("Scene","SceneTreeDock",true,false)[0].get_child(2).get_child(1).get_child(0).get_child(0)
 	_scene_tabs = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(1).get_child(1).get_child(1).get_child(0).get_child(0).get_child(0).get_child(0).get_child(0).get_child(0).get_child(0).get_child(0)
-	
+
 	_custom_project_menu = _instantiate_custom_menu(CUSTOM_PROJECT_MENU, 2, "Project")
-	
+
 	_custom_help_menu = _instantiate_custom_menu(CUSTOM_HELP_MENU, 6, "Help")
-	
+
 	if(!FileAccess.file_exists("res://addons/oip_ui/build.txt")):
 		var msBuildPanel = find_build_panel(get_parent())
 		callable = Callable(msBuildPanel,"BuildProject")
 		get_tree().root.connect("ready",Callable(self,"build"))
 		var file = FileAccess.open("res://addons/oip_ui/build.txt",FileAccess.WRITE)
 		file.store_string("This file was automatically generated. Do not delete")
-		
-	
+
+
 	_toggle_native_mode(false)
-	
+
 	_run_bar = RUN_BAR.instantiate()
 	_title_bar.add_child(_run_bar)
 	_title_bar.move_child(_run_bar, 2)
-	
+
 	_title_bar.add_child(_empty_margin)
 	_title_bar.move_child(_empty_margin, 4)
-	
+
 	_toggle_view = TOGGLE_VIEW.instantiate()
 	_title_bar.add_child(_toggle_view)
-	
+
 	_empty_margin.custom_minimum_size = Vector2(165, 0)
-	
+
 	_editor_popup_menu.add_separator()
 	_editor_popup_menu.add_check_item("Toggle Godot Native UI")
 	_editor_popup_menu.id_pressed.connect(_on_editor_popup_id_pressed)
-	
+
 	_custom_project_menu.id_pressed.connect(_on_custom_project_menu_id_pressed)
 	_custom_help_menu.id_pressed.connect(_on_custom_help_menu_id_pressed)
-	
+
 	if(EditorInterface.has_method("set_simulation_started")):
 		var button = Button.new()
 		button.text = "New Simulation"
@@ -118,39 +118,39 @@ func _enter_tree() -> void:
 		_create_root_vbox.add_child(button)
 		_create_root_vbox.move_child(button,0)
 		_create_root_vbox.move_child(_create_root_vbox.get_child(1),2)
-	
+
 	EditorInterface.get_editor_settings().set_setting("interface/editor/update_continuously",true)
-	
+
 func _exit_tree() -> void:
 	_center_buttons.visible = true
-	
+
 	if _run_bar:
 		_run_bar.queue_free()
-	
+
 	if _toggle_view:
 		_toggle_view.queue_free()
-	
+
 	if _empty_margin:
 		_empty_margin.queue_free()
-	
+
 	_editor_popup_menu.remove_item(13)
 	_editor_popup_menu.remove_item(13)
-	
+
 	if _editor_popup_menu.id_pressed.is_connected(_on_editor_popup_id_pressed):
 		_editor_popup_menu.id_pressed.disconnect(_on_editor_popup_id_pressed)
-	
+
 	if _custom_project_menu.id_pressed.is_connected(_on_custom_project_menu_id_pressed):
 		_custom_project_menu.id_pressed.disconnect(_on_custom_project_menu_id_pressed)
-	
+
 	if _custom_help_menu.id_pressed.is_connected(_on_custom_help_menu_id_pressed):
 		_custom_help_menu.id_pressed.disconnect(_on_custom_help_menu_id_pressed)
-	
+
 	if _custom_project_menu:
 		_custom_project_menu.queue_free()
-	
+
 	if _custom_help_menu:
 		_custom_help_menu.queue_free()
-	
+
 	_toggle_native_mode(true)
 
 func _new_simulation_btn_pressed():
@@ -168,10 +168,10 @@ func _remove_new_simulation():
 	var script = EditorScript.new()
 	script.call("remove_root_node")
 
-func _toggle_native_mode(native_mode: bool) -> void:	
+func _toggle_native_mode(native_mode: bool) -> void:
 	if !native_mode and get_node_or_null("/root/SimulationEvents"):
 		EditorInterface.set_main_screen_editor("3D")
-	
+
 	if _custom_project_menu and _custom_help_menu:
 		_menu_bar.set_menu_hidden(1, !native_mode)
 		_menu_bar.set_menu_hidden(2, native_mode)
@@ -181,26 +181,26 @@ func _toggle_native_mode(native_mode: bool) -> void:
 	else:
 		for menu in _menu_bar.get_children():
 			_menu_bar.set_menu_hidden(menu.get_index(), false)
-	
+
 	if _run_bar:
 		_run_bar.visible = !native_mode
 	if _toggle_view:
 		_toggle_view.visible = !native_mode
 	_center_buttons.visible = native_mode
-	
+
 	_editor_run_bar_container.visible = native_mode
 	_renderer_selection.visible = native_mode
-	
+
 	_debugger_button.visible = native_mode
 	_audio_button.visible = native_mode
 	_animation_button.visible = native_mode
-	
+
 	_set_original_popup_menu(native_mode, _project_popup_menu, _custom_project_menu, "Project")
 	_set_original_popup_menu(native_mode, _help_popup_menu, _custom_help_menu, "Help")
-	
+
 	_separator.visible = native_mode
 	_camera_button.visible = native_mode
-	
+
 	_empty_margin.visible = !native_mode
 
 
@@ -258,8 +258,8 @@ func create_inherited_scene(_inherits: PackedScene, _root_name: String = "") -> 
 	if(_root_name == ""):
 		_root_name = _inherits._bundled["names"][0];
 	var scene := PackedScene.new();
-	scene._bundled = {"base_scene": 0, "conn_count": 0, "conns": [], "editable_instances": [], 
-					"names": [_root_name], "node_count": 1, "node_paths": [], 
-					"nodes": [-1, -1, 2147483647, 0, -1, 0, 0], 
+	scene._bundled = {"base_scene": 0, "conn_count": 0, "conns": [], "editable_instances": [],
+					"names": [_root_name], "node_count": 1, "node_paths": [],
+					"nodes": [-1, -1, 2147483647, 0, -1, 0, 0],
 					"variants": [_inherits], "version": 2};
 	return scene;
