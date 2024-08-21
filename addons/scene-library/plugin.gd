472fecb789d710308641bb2b92a1bf0473dc2f27
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2024-08-21 12:47:01 -0400

    Update scene-library to v0.6

--- addons/scene-library/plugin.gd
+++ addons/scene-library/plugin.gd
@@ -5,22 +5,27 @@
 extends EditorPlugin
 
 
-var _editor : Control = null
-var _button : Button = null
+const SceneLibrary = preload("res://addons/scene-library/scripts/scene_library.gd")
 
-@warning_ignore("unsafe_method_access", "unsafe_property_access", "return_value_discarded")
+const BUTTON_NAME: String = "Parts"
+
+
+var _editor: SceneLibrary = null
+var _button: Button = null
+
+
 func _enter_tree() -> void:
-	_editor = load("res://addons/scene-library/scripts/scene_library.gd").new()
-	_editor.library_saved.connect(_mark_saved)
-	_editor.library_unsaved.connect(_mark_unsaved)
+	_editor = SceneLibrary.new()
+	_button = add_control_to_bottom_panel(_editor, BUTTON_NAME)
 	_editor.open_asset_request.connect(_on_open_asset_request)
 	_editor.show_in_file_system_request.connect(_on_show_in_file_system_request)
 	_editor.show_in_file_manager_request.connect(_on_show_in_file_manager_request)
-	_button = add_control_to_bottom_panel(_editor, "Parts")
+	_editor.library_saved.connect(_on_library_saved)
+	_editor.library_unsaved.connect(_on_library_unsaved)
 
 	get_parent().connect(&"scene_saved", _editor.handle_scene_saved)
-	get_editor_interface().get_file_system_dock().files_moved.connect(_editor.handle_file_moved)
-	get_editor_interface().get_file_system_dock().file_removed.connect(_editor.handle_file_removed)
+	EditorInterface.get_file_system_dock().files_moved.connect(_editor.handle_file_moved)
+	EditorInterface.get_file_system_dock().file_removed.connect(_editor.handle_file_removed)
 
 
 func _exit_tree() -> void:
@@ -28,20 +33,30 @@ func _exit_tree() -> void:
 	_editor.queue_free()
 
 
-func _mark_saved() -> void:
-	_button.set_text("Parts")
+func _save_external_data() -> void:
+	if _editor.is_saved():
+		return
+
+	var library_path: String = _editor.get_current_library_path()
+	if library_path.is_empty():
+		return
+
+	_editor.save_library(library_path)
 
 
-func _mark_unsaved() -> void:
-	_button.set_text("Parts(*)")
+func _on_library_saved() -> void:
+	_button.set_text(BUTTON_NAME)
+
+func _on_library_unsaved() -> void:
+	_button.set_text(BUTTON_NAME + "(*)")
 
 
 func _on_open_asset_request(path: String) -> void:
-	get_editor_interface().open_scene_from_path(path)
+	EditorInterface.open_scene_from_path(path)
 
 
 func _on_show_in_file_system_request(path: String) -> void:
-	get_editor_interface().get_file_system_dock().navigate_to_path(path)
+	EditorInterface.get_file_system_dock().navigate_to_path(path)
 
 
 func _on_show_in_file_manager_request(path: String) -> void:
