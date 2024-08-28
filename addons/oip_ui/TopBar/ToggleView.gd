340ec44e5a85baa23f811bfe475fa4ea04f29ef4
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2024-08-28 10:32:30 -0400

    Modify button to toggle editor main screen depending on which one is currently visible

--- addons/oip_ui/TopBar/ToggleView.gd
+++ addons/oip_ui/TopBar/ToggleView.gd
@@ -6,17 +6,23 @@ extends HBoxContainer
 
 @onready var _button: Button = $Button
 
-
 func _ready() -> void:
-	_button.toggled.connect(func (toggled_on: bool) -> void:
-		if toggled_on:
-			_button.icon = texture_script
-			_button.text = "Script"
-			EditorInterface.set_main_screen_editor("Script")
-		else:
-			_button.icon = texture_3d
-			_button.text = "3D"
-			EditorInterface.set_main_screen_editor("3D")
-	)
+	_button.pressed.connect(self._button_pressed)
+
+func _button_pressed():
+	var window = _get_visible_window()
 	
-	_button.button_pressed = false
+	if window.contains("WindowWrapper"):
+		_button.icon = texture_3d
+		_button.text = "3D"
+		EditorInterface.set_main_screen_editor("3D")
+	else:
+		_button.icon = texture_script
+		_button.text = "Script"
+		EditorInterface.set_main_screen_editor("Script")
+
+func _get_visible_window() -> String:
+	for window in EditorInterface.get_editor_main_screen().get_children():
+		if window.visible == true:
+			return window.name
+	return ""
