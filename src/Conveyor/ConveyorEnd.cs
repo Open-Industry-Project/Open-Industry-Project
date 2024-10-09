be0b60cc988e13e3740ccc2a021b5c7e64d959ee
Robin Cooper <robin.cooper@autStand.com>
2024-10-09 16:31:14 -0400

    Whitespace

--- src/Conveyor/ConveyorEnd.cs
+++ src/Conveyor/ConveyorEnd.cs
@@ -6,50 +6,50 @@ public partial class ConveyorEnd : Node3D
 {
 	double beltPosition = 0.0;
 	bool running = false;
-	
+
 	RigidBody3D rigidBody;
 	MeshInstance3D mesh;
 	public ShaderMaterial beltMaterial;
 	Shader beltShader;
-	
+
 	IConveyor owner;
 	Root main;
 
 	float prevScaleX;
-	
+
 	public override void _Ready()
 	{
 		rigidBody = GetNode<RigidBody3D>("RigidBody3D");
-		
+
 		mesh = GetNode<MeshInstance3D>("MeshInstance3D");
 		mesh.Mesh = mesh.Mesh.Duplicate() as Mesh;
 		beltMaterial = mesh.Mesh.SurfaceGetMaterial(0).Duplicate() as ShaderMaterial;
 		mesh.Mesh.SurfaceSetMaterial(0, beltMaterial);
 		beltShader = beltMaterial.Shader.Duplicate() as Shader;
 		beltMaterial.Shader = beltShader;
-		
+
 		owner = Owner as IConveyor;
 	}
 
-    public override void _Process(double delta)
-    {
+	public override void _Process(double delta)
+	{
 		if (owner == null) return;
 
-        if (owner.Scale.X != prevScaleX)
+		if (owner.Scale.X != prevScaleX)
 		{
-            Scale = new Vector3(1 / owner.Scale.X, 1, 1);
-            prevScaleX = owner.Scale.X;
-        }
-    }
+			Scale = new Vector3(1 / owner.Scale.X, 1, 1);
+			prevScaleX = owner.Scale.X;
+		}
+	}
 
-    public override void _PhysicsProcess(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
 		if (owner != null)
 		{
-            RemakeMesh(owner);
-            
+			RemakeMesh(owner);
+
 			if (owner.Main == null) return;
-		
+
 			if (owner.Main.Start)
 			{
 				Vector3 localFront = -GlobalTransform.Basis.Z.Normalized();
@@ -64,13 +64,13 @@ public partial class ConveyorEnd : Node3D
 				rigidBody.Rotation = Vector3.Zero;
 				rigidBody.AngularVelocity = Vector3.Zero;
 			}
-			
+
 			((ShaderMaterial)beltMaterial).SetShaderParameter("BeltPosition", beltPosition * Mathf.Sign(-owner.Speed));
 		}
 	}
-	
+
 	public void RemakeMesh(IConveyor conveyor)
-	{	
+	{
 		if (conveyor.Speed != 0)
 			((ShaderMaterial)beltMaterial).SetShaderParameter("Scale", Mathf.Sign(conveyor.Speed));
 	}
