b41443a0cade36616955d010e7331ae6a1ef9615
Robin Cooper <robin.cooper@autStand.com>
2024-10-14 17:12:56 -0400

    Whitespace (Roller.cs)

--- src/RollerConveyor/Roller.cs
+++ src/RollerConveyor/Roller.cs
@@ -5,24 +5,24 @@ using System;
 public partial class Roller : Node3D
 {
 	private float _speed = 1.0f;
-    public float Speed
-    {
-        get { return _speed; }
-        set { _speed = value; }
-    }
+	public float Speed
+	{
+		get { return _speed; }
+		set { _speed = value; }
+	}
 
-    public bool flipped = false;
+	public bool flipped = false;
 
 
-    MeshInstance3D meshInstance;
+	MeshInstance3D meshInstance;
 	StaticBody3D staticBody;
 	StandardMaterial3D material;
-    Root Main;
+	Root Main;
 	bool running = false;
 	float reverse = 0;
 	float direction = 0;
 
-    public override void _EnterTree()
+	public override void _EnterTree()
 	{
 		Main = GetParent().GetTree().EditedSceneRoot as Root;
 
@@ -30,11 +30,11 @@ public partial class Roller : Node3D
 		{
 			Main.SimulationStarted += OnSimulationStarted;
 			Main.SimulationEnded += OnSimulationEnded;
-        }
+		}
 
 		RotationDegrees = flipped ? new Vector3(0, 180, 0) : new Vector3(0, 0, 0);
-        direction = flipped ? -1.0f : 1.0f;
-    }
+		direction = flipped ? -1.0f : 1.0f;
+	}
 
 	public override void _ExitTree()
 	{
@@ -48,27 +48,27 @@ public partial class Roller : Node3D
 	public override void _Ready()
 	{
 		staticBody = GetNode<StaticBody3D>("StaticBody3D");
-        meshInstance = GetNode<MeshInstance3D>("MeshInstance3D");
-        material = meshInstance.Mesh.SurfaceGetMaterial(0) as StandardMaterial3D;
+		meshInstance = GetNode<MeshInstance3D>("MeshInstance3D");
+		material = meshInstance.Mesh.SurfaceGetMaterial(0) as StandardMaterial3D;
 
-        if (Main != null && Main.simulationRunning)
-        {
+		if (Main != null && Main.simulationRunning)
+		{
 			OnSimulationStarted();
-        }
-    }
+		}
+	}
 
-    public override void _Process(double delta)
-    {
-        if (running)
-        {
-            material.Uv1Offset -= new Vector3(direction * Speed * MathF.PI / 2 * (float)delta, 0, 0);
-            Vector3 localFront = GlobalTransform.Basis.Z.Normalized();
-            staticBody.ConstantAngularVelocity = direction * localFront * Speed * MathF.PI * 2;
-        }
-    }
+	public override void _Process(double delta)
+	{
+		if (running)
+		{
+			material.Uv1Offset -= new Vector3(direction * Speed * MathF.PI / 2 * (float)delta, 0, 0);
+			Vector3 localFront = GlobalTransform.Basis.Z.Normalized();
+			staticBody.ConstantAngularVelocity = direction * localFront * Speed * MathF.PI * 2;
+		}
+	}
 
 
-    void OnSimulationStarted()
+	void OnSimulationStarted()
 	{
 		running = true;
 	}
