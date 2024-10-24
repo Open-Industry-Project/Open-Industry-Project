1bcea5a1fa859b4db5faa09f6562f5e8ee0c9862
Robin Cooper <robin.cooper@autStand.com>
2024-10-24 05:27:30 -0400

    Get rid of all the Roller ownership shenanigans
    Now that everything is initialized `_OnEnterTree` or earlier, we don't need it anymore.

--- src/RollerConveyor/Rollers.cs
+++ src/RollerConveyor/Rollers.cs
@@ -8,10 +8,6 @@ public partial class Rollers : AbstractRollerContainer
 
 	float rollersDistance = 0.33f;
 
-	RollerConveyor owner;
-
-	int initialForeignRollerCount;
-
 	public Rollers()
 	{
 		LengthChanged += AddOrRemoveRollers;
@@ -24,14 +20,12 @@ public partial class Rollers : AbstractRollerContainer
 		int desiredRollerCount = roundedLength - 2;
 
 		int difference = desiredRollerCount - rollerCount;
-		int foreignRollersMissing = initialForeignRollerCount - rollerCount;
 
 		if (difference > 0)
 		{
 			for (int i = 0; i < difference; i++)
 			{
-				bool foreign = i < foreignRollersMissing;
-				SpawnRoller(foreign);
+				SpawnRoller();
 			}
 		}
 		else if (difference < 0)
@@ -45,16 +39,8 @@ public partial class Rollers : AbstractRollerContainer
 		}
 	}
 
-	public override void _EnterTree()
-	{
-		owner = GetParent() as RollerConveyor;
-		base._EnterTree();
-	}
-
 	public override void _Ready()
 	{
-		initialForeignRollerCount = GetForeignRollerCount();
-
 		FixRollers();
 	}
 
@@ -63,12 +49,10 @@ public partial class Rollers : AbstractRollerContainer
 		Scale = new(1 / parentScale.X, 1 / parentScale.Y, 1 / parentScale.Z);
 	}
 
-	void SpawnRoller(bool foreign = false)
+	void SpawnRoller()
 	{
-		if (GetParent() == null || owner == null) return;
 		Roller roller = rollerScene.Instantiate() as Roller;
 		AddChild(roller);
-		roller.Owner = foreign ? owner : GetTree().GetEditedSceneRoot();
 		roller.Position = new Vector3(rollersDistance * GetChildCount(), 0, 0);
 		OnRollerAdded(roller);
 		FixRollers();
@@ -77,20 +61,5 @@ public partial class Rollers : AbstractRollerContainer
 	void FixRollers()
 	{
 		((Roller)GetChild(0)).Position = new Vector3(rollersDistance, 0, 0);
-	}
-
-	int GetForeignRollerCount()
-	{
-		int count = 0;
-		Node editedScene = GetTree().GetEditedSceneRoot();
-		foreach (Node child in GetChildren())
-		{
-			if (child.Owner == editedScene)
-			{
-				break;
-			}
-			count++;
-		}
-		return count;
 	}
 }
