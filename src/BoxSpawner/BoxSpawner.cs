Open-Industry-Project/Open-Industry-Project

commit 9329d25b8d712fc7f04260d6d82ee069e921cab1

Convert spawners to using physics ticks and store transform data to avoid interop

src/BoxSpawner/BoxSpawner.cs
@@ -30,17 +30,23 @@ public partial class BoxSpawner : Node3D
 
 	Root Main;
 
+	Vector3 _rotation;
+	Vector3 _globalPosition;
+	Vector3 _scale;
+
     public override void _Ready()
     {
         if (Main != null)
         {
-            SetProcess(Main.simulationRunning);
+            SetPhysicsProcess(Main.simulationRunning);
         }
     }
 
     public override void _EnterTree()
     {
-        scan_interval = spawnInterval;
+		SetNotifyLocalTransform(true);
+
+		scan_interval = spawnInterval;
 
         Main = GetParent().GetTree().EditedSceneRoot as Root;
 
@@ -49,6 +55,9 @@ public partial class BoxSpawner : Node3D
             Main.SimulationStarted += OnSimulationStarted;
             Main.SimulationEnded += OnSimulationEnded;
         }
+
+		_rotation = Rotation;
+		_globalPosition = GlobalPosition;
     }
 
     public override void _ExitTree()
@@ -60,7 +69,7 @@ public partial class BoxSpawner : Node3D
         }
     }
 
-    public override void _Process(double delta)
+    public override void _PhysicsProcess(double delta)
 	{
 		if (Main == null || Disable) return;
 		
@@ -85,12 +94,12 @@ public partial class BoxSpawner : Node3D
 		}
 		else
 		{
-            box.Scale = Scale;
+			box.Scale = _scale;
         }
 
-        box.Rotation = Rotation;
-        box.Position = GlobalPosition;
-        box.instanced = true;
+		box.Rotation = _rotation;
+		box.Position = _globalPosition;
+		box.instanced = true;
 
         AddChild(box, forceReadableName:true);
 		box.Owner = Main;
@@ -103,12 +112,22 @@ public partial class BoxSpawner : Node3D
 
 	void OnSimulationStarted()
 	{
-		SetProcess(true);
+		SetPhysicsProcess(true);
 		scan_interval = spawnInterval;
 	}
 	
 	void OnSimulationEnded()
 	{
-		SetProcess(false);
+		SetPhysicsProcess(false);
+	}
+
+	public override void _Notification(int what)
+	{
+		if (what == NotificationLocalTransformChanged)
+		{
+			_scale = Scale;
+			_rotation = Rotation;
+			_globalPosition = GlobalPosition;
+		}
 	}
 }

src/PalletSpawner/PalletSpawner.cs
@@ -26,17 +26,22 @@ public partial class PalletSpawner : Node3D
 
 	Root Main;
 
-    public override void _Ready()
+	Vector3 _rotation;
+	Vector3 _globalPosition;
+
+	public override void _Ready()
     {
         if (Main != null)
         {
-            SetProcess(Main.simulationRunning);
+            SetPhysicsProcess(Main.simulationRunning);
         }
     }
 
     public override void _EnterTree()
     {
-        scan_interval = spawnInterval;
+		SetNotifyLocalTransform(true);
+
+		scan_interval = spawnInterval;
 
         Main = GetParent().GetTree().EditedSceneRoot as Root;
 
@@ -56,7 +61,7 @@ public partial class PalletSpawner : Node3D
         }
     }
 
-    public override void _Process(double delta)
+    public override void _PhysicsProcess(double delta)
 	{
 		if (Main == null || Disable) return;
 
@@ -72,8 +77,8 @@ public partial class PalletSpawner : Node3D
 	{
 		var pallet = (Pallet)scene.Instantiate();
 
-        pallet.Rotation = Rotation;
-        pallet.Position = GlobalPosition;
+        pallet.Rotation = _rotation;
+        pallet.Position = _globalPosition;
         pallet.instanced = true;
 
         AddChild(pallet, forceReadableName: true);
@@ -82,12 +87,21 @@ public partial class PalletSpawner : Node3D
 
 	void OnSimulationStarted()
 	{
-		SetProcess(true);
+		SetPhysicsProcess(true);
 		scan_interval = spawnInterval;
 	}
 
 	void OnSimulationEnded()
 	{
-		SetProcess(false);
+		SetPhysicsProcess(false);
+	}
+
+	public override void _Notification(int what)
+	{
+		if (what == NotificationLocalTransformChanged)
+		{
+			_rotation = Rotation;
+			_globalPosition = GlobalPosition;
+		}
 	}
 }

commit 1c5cb83c3ee4ef151ed1f4666e1c9f6f9e8c7129

Add missing initialization for `_scale` to fix Jolt errors
Fixes regression introduced in 9329d25 that affected BoxSpawners without SpawnRandomScale enabled.

src/BoxSpawner/BoxSpawner.cs
@@ -56,6 +56,7 @@ public partial class BoxSpawner : Node3D
 			Main.SimulationEnded += OnSimulationEnded;
 		}
 
+		_scale = Scale;
 		_rotation = Rotation;
 		_globalPosition = GlobalPosition;
 	}