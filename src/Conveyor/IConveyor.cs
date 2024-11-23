c36198228ca7a05538f69c82302ba9a314a83369
Robin Cooper <robin.cooper@autStand.com>
2024-11-23 13:25:47 -0500

    Remove unrelated stuff from IConveyor

--- src/Conveyor/IConveyor.cs
+++ src/Conveyor/IConveyor.cs
@@ -4,7 +4,4 @@ using System;
 public interface IConveyor
 {
 	public float Speed { get; set; }
-	public Root Main { get; set; }
-	public Transform3D GlobalTransform { get; set; }
-	public Vector3 Scale { get; set; }
 }
