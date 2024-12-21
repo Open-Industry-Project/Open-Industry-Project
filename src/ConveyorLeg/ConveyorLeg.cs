6d457ce21277937f74dc3c63d86c049542e71296
Robin Cooper <robin.cooper@autStand.com>
2024-12-21 00:06:14 -0500

    Only update ConveyorLeg on transform change

--- src/ConveyorLeg/ConveyorLeg.cs
+++ src/ConveyorLeg/ConveyorLeg.cs
@@ -15,12 +15,9 @@ public partial class ConveyorLeg : Node3D
 		set
 		{
 			grabsRotation = value;
-			SetPhysicsProcess(true);
+			OnGrabsUpdated();
 		}
 	}
-
-	float nodeScaleY = 1.0f;
-	float nodeScaleZ = 1.0f;
 
 	MeshInstance3D grab1;
 	MeshInstance3D grab2;
@@ -35,40 +32,37 @@ public partial class ConveyorLeg : Node3D
 
 	Vector3 prevScale;
 
-	public override void _Ready()
+	public ConveyorLeg()
 	{
-		grab1 = GetNode<MeshInstance3D>("Ends/LegsTop1/LegsGrab1");
-		grab2 = GetNode<MeshInstance3D>("Ends/LegsTop2/LegsGrab2");
-
-		legsSidesMesh1 = GetNode<MeshInstance3D>("Sides/LegsSide1");
-		legsSidesMesh2 = GetNode<MeshInstance3D>("Sides/LegsSide2");
-		legsSidesMaterial = legsSidesMesh1.Mesh.SurfaceGetMaterial(0) as ShaderMaterial;
-		legsSidesMesh1.Mesh.SurfaceSetMaterial(0, legsSidesMaterial);
-		legsBars = GetNode<LegBars>("LegsBars");
-		ends = GetNode<Node3D>("Ends");
-
 		SetNotifyLocalTransform(true);
 	}
 
-	public override void _Process(double delta)
+	void SetupReferences()
 	{
-		if (Scale.Y >= 1.0f)
-			nodeScaleY = Scale.Y;
+		legsSidesMesh1 ??= GetNode<MeshInstance3D>("Sides/LegsSide1");
+		legsSidesMesh2 ??= GetNode<MeshInstance3D>("Sides/LegsSide2");
+		legsSidesMaterial ??= legsSidesMesh1.Mesh.SurfaceGetMaterial(0) as ShaderMaterial;
+		legsSidesMesh1.Mesh.SurfaceSetMaterial(0, legsSidesMaterial);
+		legsBars ??= GetNode<LegBars>("LegsBars");
+		ends ??= GetNode<Node3D>("Ends");
 
-		nodeScaleZ = Scale.Z;
-
-		Vector3 newScale = new(1, nodeScaleY, nodeScaleZ);
-
-		if(Scale != newScale)
-		{
-			Scale = new Vector3(1, nodeScaleY, nodeScaleZ);
-		}
-		SetProcess(false);
+		grab1 ??= GetNode<MeshInstance3D>("Ends/LegsTop1/LegsGrab1");
+		grab2 ??= GetNode<MeshInstance3D>("Ends/LegsTop2/LegsGrab2");
 	}
 
-	public override void _PhysicsProcess(double delta)
+	Vector3 ConstrainScale()
+	{
+		float nodeScaleY = float.Max(1.0f, Scale.Y);
+		float nodeScaleZ = Scale.Z;
+		Vector3 newScale = new(1, nodeScaleY, nodeScaleZ);
+		return newScale;
+	}
+
+	void OnScaleChanged()
 	{
 		if(Scale == prevScale) return;
+
+		SetupReferences();
 
 		if (legsSidesMaterial != null)
 			legsSidesMaterial.SetShaderParameter("Scale", Scale.Y);
@@ -84,22 +78,30 @@ public partial class ConveyorLeg : Node3D
 		legsSidesMesh1.Scale = new Vector3(1 / Scale.X, 1, 1 / Scale.Z);
 		legsSidesMesh2.Scale = new Vector3(1 / Scale.X, 1, 1 / Scale.Z);
 
-		grab1.RotationDegrees = new Vector3(0, 0, grabsRotation);
-		grab2.RotationDegrees = new Vector3(0, 0, -grabsRotation);
-
-		grab1.Scale = Vector3.One;
-		grab2.Scale = Vector3.One;
-
 		prevScale = Scale;
-		SetPhysicsProcess(false);
+	}
+
+	void OnGrabsUpdated()
+	{
+		SetupReferences();
+
+		grab1?.SetRotationDegrees(new Vector3(0, 0, grabsRotation));
+		grab2?.SetRotationDegrees(new Vector3(0, 0, -grabsRotation));
+
+		grab1?.SetScale(Vector3.One);
+		grab2?.SetScale(Vector3.One);
 	}
 
 	public override void _Notification(int what)
 	{
 		if (what == NotificationLocalTransformChanged)
 		{
-			SetProcess(true);
-			SetPhysicsProcess(true);
+			Vector3 constrainedScale = ConstrainScale();
+			if (constrainedScale != Scale)
+			{
+				Scale = constrainedScale;
+			}
+			OnScaleChanged();
 		}
 	}
 }
