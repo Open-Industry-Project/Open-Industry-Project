Open-Industry-Project/Open-Industry-Project

commit fe1a63b1e07ba5bb8a8413a44d30c37a9ba63eae

Set RollerConveyor's base width to 1m
Each unit of Z scale now equals one meter of width (instead of 2m like before).

Default width remains 2m.

src/RollerConveyor/AbstractRollerContainer.cs
@@ -29,19 +29,26 @@ public abstract partial class AbstractRollerContainer : Node3D
 		RollerRemoved += HandleRollerRemoved;
 	}
 
-	public override void _EnterTree()
+	public override void _Notification(int what)
 	{
-		foreach (Roller roller in GetRollers())
+		if (what == NotificationSceneInstantiated)
 		{
-			EmitSignalRollerAdded(roller);
+			OnSceneInstantiated();
 		}
+		base._Notification(what);
 	}
 
-	public override void _ExitTree()
+	protected virtual void OnSceneInstantiated()
 	{
 		foreach (Roller roller in GetRollers())
 		{
+			// If LengthChanged has already been fired, then we've already
+			// added and subscribed some Rollers, but we still need to
+			// subscribe the original ones. To ensure that each Roller is
+			// only subscribed once, we're going to unsubscribe them all,
+			// then then subscribe them.
 			EmitSignalRollerRemoved(roller);
+			EmitSignalRollerAdded(roller);
 		}
 	}
 

src/RollerConveyor/RollerConveyor.cs
@@ -64,12 +64,13 @@ public partial class RollerConveyor : Node3D, IRollerConveyor
 	float nodeScaleZ = 1.0f;
 	Vector3 lastScale = Vector3.One;
 	float lastLength = 1f;
-	float lastWidth = baseWidth;
+	float lastWidth = float.NaN;
 	Transform3D previousTransform = Transform3D.Identity;
 
 	const float radius = 0.12f;
 	const float circumference = 2f * MathF.PI * radius;
-	const float baseWidth = 2f;
+	const float baseWidth = 1f;
+	const float frameBaseWidth = 2f;
 
 	Material metalMaterial;
 	Rollers rollers;
@@ -193,11 +194,18 @@ public partial class RollerConveyor : Node3D, IRollerConveyor
 		rollers = GetNodeOrNull<Rollers>("Rollers");
 		ends = GetNodeOrNull<Node3D>("Ends");
 
+		// Rollers isn't an instance, so it can't receive NotificationSceneInstantiated.
+		// We'll let it piggyback on our notification instead.
+		rollers.OnSceneInstantiated();
+
 		SetupRollerContainer(rollers);
 		foreach (RollerConveyorEnd end in ends.GetChildren())
 		{
 			SetupRollerContainer(end);
 		}
+		UpdateScale();
+		UpdateWidth();
+		UpdateLength();
 	}
 
 	void OnSimulationStarted()
@@ -346,7 +354,7 @@ public partial class RollerConveyor : Node3D, IRollerConveyor
 		// TODO does this need a unique? Might already be done in the inspector.
 		var meshInstance1 = GetNode<Node3D>("ConvRoller/ConvRollerL");
 		var meshInstance2 = GetNode<Node3D>("ConvRoller/ConvRollerR");
-		meshInstance1.Scale = new Vector3(1f, 1f, baseWidth / width);
-		meshInstance2.Scale = new Vector3(1f, 1f, baseWidth / width);
+		meshInstance1.Scale = new Vector3(1f, 1f, frameBaseWidth * baseWidth / width);
+		meshInstance2.Scale = new Vector3(1f, 1f, frameBaseWidth * baseWidth / width);
 	}
 }

src/RollerConveyor/RollerConveyorEnd.cs
@@ -21,13 +21,13 @@ public partial class RollerConveyorEnd : AbstractRollerContainer
 		WidthChanged += SetEndsSeparation;
 	}
 
-	public override void _EnterTree()
+	protected override void OnSceneInstantiated()
 	{
 		roller = GetNode<Roller>("Roller");
-		base._EnterTree();
+		base.OnSceneInstantiated();
 	}
 
-	public override void _ExitTree()
+	~RollerConveyorEnd()
 	{
 		WidthChanged -= SetEndsSeparation;
 	}

src/RollerConveyor/Rollers.cs
@@ -13,18 +13,9 @@ public partial class Rollers : AbstractRollerContainer
 		LengthChanged += AddOrRemoveRollers;
 	}
 
-	public override void _EnterTree()
+	public new void OnSceneInstantiated()
 	{
-		foreach (Roller roller in GetRollers())
-		{
-			// If LengthChanged has already been fired, then we've already
-			// added and subscribed some Rollers, but we still need to
-			// subscribe the original ones. To ensure that each Roller is
-			// only subscribed once, we're going to unsubscribe them all,
-			// then let the base class resubscribe them all.
-			EmitSignalRollerRemoved(roller);
-		}
-		base._EnterTree();
+		base.OnSceneInstantiated();
 	}
 
 	private void AddOrRemoveRollers(float conveyorLength)

parts/RollerConveyor.tscn
@@ -8,15 +8,15 @@
 
 [node name="RollerConveyor" type="Node3D"]
 process_mode = 3
-transform = Transform3D(1.02487, 0, 0, 0, 1, -1.50996e-07, 0, 1.50996e-07, 1, 0, 0, 0)
+transform = Transform3D(1.025, 0, 0, 0, 1, -3.01992e-07, 0, 1.50996e-07, 2, 0, 0, 0)
 script = ExtResource("1_vnty6")
-Speed = 2.0
 metadata/_edit_group_ = true
 
 [node name="ConvRoller" parent="." instance=ExtResource("3_sau1a")]
+transform = Transform3D(1, 0, 0, 0, 1, 0, 0, 0, 0.5, 0, 0, 0)
 
 [node name="Rollers" type="Node3D" parent="."]
-transform = Transform3D(0.975733, 0, 0, 0, 1, -1.42109e-14, 0, 1.42109e-14, 1, -0.5, 0.17, 3.55271e-15)
+transform = Transform3D(0.97561, 0, 0, 0, 1, -7.10545e-15, 0, 1.42109e-14, 0.5, -0.5, 0.17, 3.55271e-15)
 script = ExtResource("3_peu56")
 rollerScene = ExtResource("2_v7a13")
 
@@ -30,8 +30,8 @@ transform = Transform3D(1, 0, 0, 0, 1, 0, 0, 0, 1, 0.66, 0, 0)
 transform = Transform3D(1, 0, 0, 0, 1, 1.42109e-14, 0, -1.42109e-14, 1, 0, 0, 0)
 
 [node name="RollerConveyorEnd" parent="Ends" instance=ExtResource("6_fn5jx")]
-transform = Transform3D(0.975733, 0, 0, 0, 1, 0, 0, 0, 1, 0.5, 0, 0)
+transform = Transform3D(0.97561, 0, 0, 0, 1, 0, 0, 0, 0.5, 0.5, 0, 0)
 
 [node name="RollerConveyorEnd2" parent="Ends" instance=ExtResource("6_fn5jx")]
-transform = Transform3D(-0.975733, 6.47981e-14, 1.50996e-07, -2.57603e-14, 1, -6.03984e-07, -1.47332e-07, -6.03984e-07, -1, -0.5, 0, 0)
+transform = Transform3D(-0.97561, 6.47981e-14, 7.54979e-08, -2.5757e-14, 1, -3.01992e-07, -1.47313e-07, -6.03984e-07, -0.5, -0.5, 0, 0)
 flipped = true

commit 3e24c0781b808182b8697b2d05e4746d07e3d2c2

Fix regression where pre-existing rollers don't spin
Regression introduced in fe1a63b1.

src/RollerConveyor/AbstractRollerContainer.cs
@@ -29,16 +29,7 @@ public abstract partial class AbstractRollerContainer : Node3D
 		RollerRemoved += HandleRollerRemoved;
 	}
 
-	public override void _Notification(int what)
-	{
-		if (what == NotificationSceneInstantiated)
-		{
-			OnSceneInstantiated();
-		}
-		base._Notification(what);
-	}
-
-	protected virtual void OnSceneInstantiated()
+	internal virtual void SetupExistingRollers()
 	{
 		foreach (Roller roller in GetRollers())
 		{

src/RollerConveyor/RollerConveyor.cs
@@ -195,18 +195,17 @@ public partial class RollerConveyor : Node3D, IRollerConveyor
 		rollers = GetNodeOrNull<Rollers>("Rollers");
 		ends = GetNodeOrNull<Node3D>("Ends");
 
-		// Rollers isn't an instance, so it can't receive NotificationSceneInstantiated.
-		// We'll let it piggyback on our notification instead.
-		rollers.OnSceneInstantiated();
-
 		SetupRollerContainer(rollers);
 		foreach (RollerConveyorEnd end in ends.GetChildren())
 		{
 			SetupRollerContainer(end);
 		}
+
+		// In case transform was changed before scene was instantiated somehow.
 		UpdateScale();
 		UpdateWidth();
 		UpdateLength();
+		UpdateSize();
 	}
 
 	void OnSimulationStarted()
@@ -244,6 +243,8 @@ public partial class RollerConveyor : Node3D, IRollerConveyor
 		ScaleChanged += rollers.OnOwnerScaleChanged;
 		WidthChanged += rollers.SetWidth;
 		LengthChanged += rollers.SetLength;
+
+		rollers.SetupExistingRollers();
 
 		rollers.SetRollerSkewAngle(skewAngle);
 		rollers.OnOwnerScaleChanged(Scale);

src/RollerConveyor/RollerConveyorEnd.cs
@@ -21,10 +21,10 @@ public partial class RollerConveyorEnd : AbstractRollerContainer
 		WidthChanged += SetEndsSeparation;
 	}
 
-	protected override void OnSceneInstantiated()
+	internal override void SetupExistingRollers()
 	{
 		roller = GetNode<Roller>("Roller");
-		base.OnSceneInstantiated();
+		base.SetupExistingRollers();
 	}
 
 	~RollerConveyorEnd()

src/RollerConveyor/Rollers.cs
@@ -13,11 +13,6 @@ public partial class Rollers : AbstractRollerContainer
 		LengthChanged += AddOrRemoveRollers;
 	}
 
-	public new void OnSceneInstantiated()
-	{
-		base.OnSceneInstantiated();
-	}
-
 	private void AddOrRemoveRollers(float conveyorLength)
 	{
 		int roundedLength = Mathf.RoundToInt(conveyorLength / rollersDistance) + 1;