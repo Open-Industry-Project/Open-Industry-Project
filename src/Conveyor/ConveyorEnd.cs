45f49db39584ba4da1cd2f60b6a7aa350d698d71
Robin Cooper <robin.cooper@autStand.com>
2025-01-14 20:06:27 -0500

    Remove `_Process` usage from BeltConveyor and CurvedBeltConveyor
    React to Transform changes via NotificationLocalTransformChanged instead.
    
    Set BeltConveyor and CurvedBeltConveyor ProcessMode to Pausable

--- src/Conveyor/ConveyorEnd.cs
+++ src/Conveyor/ConveyorEnd.cs
@@ -4,7 +4,14 @@ using System;
 [Tool]
 public partial class ConveyorEnd : Node3D
 {
-	public float Speed;
+	public float Speed { get => speed; set
+		{
+			speed = value;
+			UpdateBeltMaterialScale();
+			UpdateBeltMaterialPosition();
+		}
+	}
+	float speed = 2.0f;
 	double beltPosition = 0.0;
 	bool running = false;
 
@@ -13,7 +20,6 @@ public partial class ConveyorEnd : Node3D
 	public ShaderMaterial beltMaterial;
 	Shader beltShader;
 
-	BeltConveyor owner;
 	Root main;
 
 	float prevScaleX;
@@ -29,52 +35,51 @@ public partial class ConveyorEnd : Node3D
 		beltShader = beltMaterial.Shader.Duplicate() as Shader;
 		beltMaterial.Shader = beltShader;
 
-		owner = Owner as BeltConveyor;
 		main = GetTree().EditedSceneRoot as Root;
 	}
 
-	public override void _Process(double delta)
+	public void OnOwnerScaleChanged(Vector3 newOwnerScale)
 	{
-		if (owner == null) return;
-
-		if (owner.Scale.X != prevScaleX)
+		if (newOwnerScale.X != prevScaleX)
 		{
-			Scale = new Vector3(1 / owner.Scale.X, 1, 1);
-			prevScaleX = owner.Scale.X;
+			Scale = new Vector3(1 / newOwnerScale.X, 1, 1);
+			prevScaleX = newOwnerScale.X;
 		}
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		if (owner != null)
+		if (main == null) return;
+
+		if (main.Start)
 		{
-			RemakeMesh(owner);
-
-			if (main == null) return;
-
-			if (main.Start)
-			{
-				Vector3 localFront = GlobalTransform.Basis.Z.Normalized();
-				if (!owner.Main.simulationPaused)
-					beltPosition += Speed * delta;
-				if (beltPosition >= 1.0)
-					beltPosition = 0.0;
-				const float radius = 0.25f;
-				staticBody.ConstantAngularVelocity = localFront * Speed / radius;
-			}
-			else
-			{
-				beltPosition = 0; // Remove from here when signals are fixed.
-				staticBody.ConstantAngularVelocity = Vector3.Zero;
-			}
-
-			((ShaderMaterial)beltMaterial).SetShaderParameter("BeltPosition", beltPosition * Mathf.Sign(-Speed));
+			Vector3 localFront = GlobalTransform.Basis.Z.Normalized();
+			if (!main.simulationPaused)
+				beltPosition += Speed * delta;
+			if (beltPosition >= 1.0)
+				beltPosition = 0.0;
+			const float radius = 0.25f;
+			staticBody.ConstantAngularVelocity = localFront * Speed / radius;
+			UpdateBeltMaterialPosition();
+		}
+		else
+		{
+			beltPosition = 0; // Remove from here when signals are fixed.
+			staticBody.ConstantAngularVelocity = Vector3.Zero;
+			UpdateBeltMaterialPosition();
 		}
 	}
 
-	public void RemakeMesh(IConveyor conveyor)
+	public void UpdateBeltMaterialScale()
 	{
-		if (conveyor.Speed != 0)
-			((ShaderMaterial)beltMaterial).SetShaderParameter("Scale", Mathf.Sign(conveyor.Speed));
+		if (Speed != 0)
+		{
+			((ShaderMaterial)beltMaterial)?.SetShaderParameter("Scale", Mathf.Sign(Speed));
+		}
+	}
+
+	public void UpdateBeltMaterialPosition()
+	{
+		((ShaderMaterial)beltMaterial)?.SetShaderParameter("BeltPosition", beltPosition * Mathf.Sign(-Speed));
 	}
 }
