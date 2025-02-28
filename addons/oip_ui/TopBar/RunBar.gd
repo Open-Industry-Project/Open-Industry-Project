2368b463720533a54ddce55b4f19a2c44a8f6f7d
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2025-02-28 14:53:22 +0400

    Fix add-on project settings being overwritten at startup

--- addons/oip_ui/TopBar/RunBar.gd
+++ addons/oip_ui/TopBar/RunBar.gd
@@ -24,7 +24,9 @@ func _enable_buttons() -> void:
 	stop_button.disabled = stop
 
 func _ready() -> void:
-	ProjectSettings.set_setting("addons/Open Industry Project/Output/Clear on Simulation Start", false)
+	if not ProjectSettings.get_setting("addons/Open Industry Project/Output/Clear on Simulation Start"):
+		ProjectSettings.set_setting("addons/Open Industry Project/Output/Clear on Simulation Start", false)
+		
 	ProjectSettings.set_as_basic("addons/Open Industry Project/Output/Clear on Simulation Start",true)
 	
 	clear_output_btn = get_tree().root.get_child(0).get_child(4).get_child(0).get_child(1).get_child(1).get_child(1).get_child(0).get_child(0).get_child(1).get_child(0).get_child(0).get_child(2).get_child(0).get_child(0)
