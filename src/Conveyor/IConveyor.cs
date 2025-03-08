13dabff79aabb55411d574dd728f78260e25efd2
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2025-03-08 16:20:43 +0400

    Convert `Despawner` to GDScript

--- src/Conveyor/IConveyor.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-using System;
-
-public interface IConveyor : IComms
-{
-	public float Speed { get; set; }
-	public Node3D AsNode3D() { return this as Node3D; }
-}
--- src/Resources/MainInstances.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-
-[Tool]
-[GlobalClass]
-public partial class MainInstances : Resource
-{
-	public Root main;
-}
