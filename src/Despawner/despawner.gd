1bbf26d8d1c6c85778c4805bd3ac7278a58ee0e6
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2025-03-08 16:20:43 +0400

    Convert `Despawner` to GDScript

---
+++ src/Despawner/despawner.gd
@@ -0,0 +1,15 @@
+@tool
+extends Area3D
+
+func _enter_tree() -> void:
+	body_entered.connect(_body_entered)
+	
+func _exit_tree() -> void:
+	body_entered.disconnect(_body_entered)
+
+func _body_entered(node: Node) -> void:
+		var _parent = node.get_parent()
+		if(_parent.has_method("select")):
+			_parent.queue_free()
+			
+		
