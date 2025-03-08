642caf76a205da85ef13dc77417d918152c46f5b
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2025-03-08 15:35:05 +0400

    Convert `BladeStop` to GDScript

--- src/PushButton/push_button.gd
+++ src/PushButton/push_button.gd
@@ -5,8 +5,8 @@ class_name PushButton
 @export var text: String = "Stop":
 	set(value):
 		text = value
-		if text_mesh:
-			text_mesh.text = text
+		if _text_mesh:
+			_text_mesh.text = text
 
 @export var toggle: bool = false:
 	set(value):
@@ -20,68 +20,55 @@ class_name PushButton
 		pushbutton = value
 		if not toggle and pushbutton:
 			reset_pushbutton()
-			var tween = get_tree().create_tween().bind_node(self)
-			tween.tween_property(button_mesh, "position", Vector3(0, 0, button_pressed_z_pos), 0.035)
+			var tween = create_tween()
+			tween.tween_property(_button_mesh, "position", Vector3(0, 0, _button_pressed_z_pos), 0.035)
 			tween.tween_interval(0.2)
-			tween.tween_property(button_mesh, "position", Vector3.ZERO, 0.02)
-		elif button_mesh:
+			tween.tween_property(_button_mesh, "position", Vector3.ZERO, 0.02)
+		elif _button_mesh:
 			if pushbutton:
-				button_mesh.position = Vector3(0, 0, button_pressed_z_pos)
+				_button_mesh.position = Vector3(0, 0, _button_pressed_z_pos)
 			else:
-				button_mesh.position = Vector3.ZERO
+				_button_mesh.position = Vector3.ZERO
 		
 
 @export var lamp: bool = false:
 	set(value):
 		lamp = value
-		SetActive(lamp)
+		if not  _button_material:
+			return
+		if value:
+			_button_material.emission_energy_multiplier = 1.0
+		else:
+			_button_material.emission_energy_multiplier = 0.0
 
 @export var button_color: Color = Color.RED:
 	set(value):
 		button_color = value
-		SetButtonColor(value)
+		if _button_material:
+			_button_material.albedo_color = value
+			_button_material.emission = value
 	
-var text_mesh_instance: MeshInstance3D
-var text_mesh: TextMesh
+var _text_mesh_instance: MeshInstance3D
+var _text_mesh: TextMesh
 
-var button_mesh: MeshInstance3D
-var button_material: StandardMaterial3D
-var button_pressed_z_pos: float = -0.04
+var _button_mesh: MeshInstance3D
+var _button_material: StandardMaterial3D
+var _button_pressed_z_pos: float = -0.04
 
-# Asynchronous function to reset pushbutton after a delay.
 func reset_pushbutton() -> void:
 	await get_tree().create_timer(0.3).timeout
 	pushbutton = false
 
 func _ready() -> void:
-	# Assign 3D text.
-	text_mesh_instance = $TextMesh
-	text_mesh = text_mesh_instance.mesh.duplicate() as TextMesh
-	text_mesh_instance.mesh = text_mesh
-	text_mesh.text = text
+	_text_mesh_instance = $TextMesh
+	_text_mesh = _text_mesh_instance.mesh.duplicate() as TextMesh
+	_text_mesh_instance.mesh = _text_mesh
+	_text_mesh.text = text
 
-	# Assign button.
-	button_mesh = $Meshes/Button
-	button_mesh.mesh = button_mesh.mesh.duplicate()
-	button_material = button_mesh.mesh.surface_get_material(0).duplicate() as StandardMaterial3D
-	button_mesh.mesh.surface_set_material(0, button_material)
-
-	# Initialize property states.
-	SetButtonColor(button_color)
-	SetActive(lamp)
+	_button_mesh = $Meshes/Button
+	_button_mesh.mesh = _button_mesh.mesh.duplicate()
+	_button_material = _button_mesh.mesh.surface_get_material(0).duplicate() as StandardMaterial3D
+	_button_mesh.mesh.surface_set_material(0, _button_material)
 
 func use() -> void:
 	pushbutton = not pushbutton
-
-func SetActive(new_value: bool) -> void:
-	if button_material == null:
-		return
-	if new_value:
-		button_material.emission_energy_multiplier = 1.0
-	else:
-		button_material.emission_energy_multiplier = 0.0
-
-func SetButtonColor(new_value: Color) -> void:
-	if button_material:
-		button_material.albedo_color = new_value
-		button_material.emission = new_value
