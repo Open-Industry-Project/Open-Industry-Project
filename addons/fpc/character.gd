091fe31dbe75fb7ff3886e0ad6c761b87042f602
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2025-03-08 23:10:37 +0400

    Remove debug menu from character script

--- addons/fpc/character.gd
+++ addons/fpc/character.gd
@@ -386,9 +386,3 @@ func _unhandled_input(event : InputEvent):
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		mouseInput.x += event.relative.x
 		mouseInput.y += event.relative.y
-	# Toggle debug menu
-	elif event is InputEventKey:
-		if event.is_released():
-			# Where we're going, we don't need InputMap
-			if event.keycode == 4194338: # F7
-				$UserInterface/DebugPanel.visible = !$UserInterface/DebugPanel.visible
