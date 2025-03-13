4440077b3bffca01a07c42dd9e6419eda28b9828
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2025-03-13 23:12:37 +0400

    Convert  Curved Side Guards to GDScript

---
+++ parts/curved_side_guards.gd
@@ -0,0 +1,29 @@
+@tool
+extends MeshInstance3D
+class_name CurvedSideGuards
+
+var shader_material: ShaderMaterial = null
+
+func _init() -> void:
+	set_notify_local_transform(true)
+
+func _ready() -> void:
+	# Duplicate the mesh and its material.
+	self.mesh = self.mesh.duplicate()
+	shader_material = self.mesh.surface_get_material(0).duplicate() as ShaderMaterial
+	self.mesh.surface_set_material(0, shader_material)
+	on_scale_changed()
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_LOCAL_TRANSFORM_CHANGED:
+		on_scale_changed()
+
+func on_scale_changed() -> void:
+	var new_scale: Vector3 = Vector3(scale.x, 1, scale.x)
+	if scale != new_scale:
+		set_notify_local_transform(false)
+		scale = new_scale
+		set_notify_local_transform(true)
+	if scale.x > 0.5:
+		if shader_material:
+			shader_material.set_shader_parameter("Scale", scale.x)
