235121d6286cd6c56a7ad057a901d53860f1a949
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2025-03-31 12:10:00 +0400

    Add OIPComms to `ColorSensor`

--- src/ColorSensor/color_sensor.gd
+++ src/ColorSensor/color_sensor.gd
@@ -1,31 +1,24 @@
 @tool
 extends Node3D
 
-var ray_marker: Marker3D
-var ray_mesh: MeshInstance3D
-var cylinder_mesh: CylinderMesh
-var ray_material: StandardMaterial3D
-
 var register_tag_ok := false
 var tag_group_init := false
 var tag_group_original: String
 
-@export var enable_comms := true
-@export var tag_group_name: String
-@export_custom(0,"tag_group_enum") var tag_groups:
+var mesh : ImmediateMesh
+var beam_mat : StandardMaterial3D = preload("uid://ntmcfd25jgpm").duplicate()
+var instance
+var scenario
+
+@export var max_range: float = 1.524
+
+@export var show_beam: bool = true:
 	set(value):
-		tag_group_name = value
-		tag_groups = value
-			
-@export var tag_name := ""	
-@export var max_range: float = 6.0
-@export var show_beam: bool :
-	set(value): 
 		show_beam = value
-		if(ray_marker):
-			ray_marker.visible = value;
+		if instance: 
+			RenderingServer.instance_set_visible(instance,show_beam)
 	
-@export var color : Color = Color.BLACK
+@export var color : Color = Color.TRANSPARENT
 		
 @export var color_map : Dictionary[Color,int] = {
 	Color.RED:1, 
@@ -39,6 +32,17 @@ var tag_group_original: String
 			OIPComms.write_int32(tag_group_name, tag_name, value)
 
 		color_value = value
+
+@export_category("Communications")
+
+@export var enable_comms := false
+@export var tag_group_name: String
+@export_custom(0,"tag_group_enum") var tag_groups:
+	set(value):
+		tag_group_name = value
+		tag_groups = value
+		
+@export var tag_name := ""
 
 func _validate_property(property: Dictionary):
 	if property.name == "color_value":
@@ -57,16 +61,15 @@ func _property_get_revert(property: StringName) -> Variant:
 	else: 
 		return
 
-func _ready() -> void:
-	ray_marker = $RayMarker
-	ray_mesh = $RayMarker/MeshInstance3D
-	cylinder_mesh = ray_mesh.mesh.duplicate() as CylinderMesh
-	ray_mesh.mesh = cylinder_mesh
-	ray_material = cylinder_mesh.material.duplicate() as StandardMaterial3D
-	cylinder_mesh.material = ray_material
-	ray_marker.visible = show_beam
-
 func _enter_tree() -> void:
+	instance = RenderingServer.instance_create()
+	scenario = get_world_3d().scenario
+	mesh = ImmediateMesh.new()
+	
+	RenderingServer.instance_set_scenario(instance, scenario)
+	RenderingServer.instance_set_base(instance, mesh)
+	RenderingServer.instance_set_visible(instance,show_beam)
+	
 	tag_group_original = tag_group_name
 	if(tag_group_name.is_empty()):
 		tag_group_name = OIPComms.get_tag_groups()[0]
@@ -74,51 +77,50 @@ func _enter_tree() -> void:
 	tag_groups = tag_group_name
 
 	SimulationEvents.simulation_started.connect(_on_simulation_started)
-	SimulationEvents.simulation_ended.connect(_on_simulation_ended)
 	OIPComms.tag_group_initialized.connect(_tag_group_initialized)
 
 func _exit_tree() -> void:
 	SimulationEvents.simulation_started.disconnect(_on_simulation_started)
-	SimulationEvents.simulation_ended.disconnect(_on_simulation_ended)
 	OIPComms.tag_group_initialized.disconnect(_tag_group_initialized)
 
 
 func _physics_process(delta: float) -> void:
-	var space_state = get_world_3d().direct_space_state
-	var start_pos = ray_marker.global_transform.origin
+	var start_pos = global_transform.translated_local(Vector3(0, 0.25, 0.42)).origin
 	var end_pos = start_pos + global_transform.basis.z * max_range
-	var query = PhysicsRayQueryParameters3D.create(start_pos, end_pos)
-	query.collision_mask = 8
+
+	var query = PhysicsRayQueryParameters3D.create(start_pos, end_pos, 8)
+	var space_state = get_world_3d().direct_space_state
 	var result = space_state.intersect_ray(query)
-	
+	var result_distance = max_range
+
 	if result.size() > 0:
+		result_distance = start_pos.distance_to(result["position"])
 		var collider : CollisionObject3D = result["collider"]
 		var mesh_instance : MeshInstance3D = collider.get_node("MeshInstance3D")
 		var material : StandardMaterial3D = mesh_instance.mesh.surface_get_material(0)
 		color = material.albedo_color
 		color_value = color_map.get(color,0)
-		var result_distance = ray_marker.global_transform.origin.distance_to(result["position"])
-		if cylinder_mesh.height != result_distance:
-				cylinder_mesh.height = result_distance
-		if ray_material.albedo_color != Color.RED:
-				ray_material.albedo_color = Color.RED
+		beam_mat.albedo_color = Color.RED
 	else:
 		color = Color.TRANSPARENT
 		color_value = 0
-		if cylinder_mesh.height != max_range:
-				cylinder_mesh.height = max_range
-		if ray_material.albedo_color != Color.GREEN:
-				ray_material.albedo_color = Color.GREEN
-	
-	ray_mesh.position = Vector3(0, 0, cylinder_mesh.height * 0.5)
+		beam_mat.albedo_color = Color.GREEN
+
+	if show_beam:
+		mesh.clear_surfaces()
+		mesh.surface_begin(Mesh.PRIMITIVE_LINES, beam_mat)
+		mesh.surface_add_vertex(start_pos)
+		
+		if color_value == 0:
+			mesh.surface_add_vertex(start_pos + global_transform.basis.z * result_distance)
+		else:
+			mesh.surface_add_vertex(start_pos + global_transform.basis.z * max_range)
+		
+		mesh.surface_end()
 
 func _on_simulation_started() -> void:
 	if enable_comms:
 		register_tag_ok = OIPComms.register_tag(tag_group_name, tag_name, 1)
-
-func _on_simulation_ended() -> void:
-	cylinder_mesh.height = max_range
-	ray_mesh.position = Vector3(0, 0, cylinder_mesh.height * 0.5)
 
 func _tag_group_initialized(_tag_group_name: String) -> void:
 	if _tag_group_name == tag_group_name:
