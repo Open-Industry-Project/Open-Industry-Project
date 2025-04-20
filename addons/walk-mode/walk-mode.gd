a78c663a67afc56637ec4d0fb768c63e139393cf
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2025-04-21 01:12:36 +0400

    Simplify walk mode transform

--- addons/walk-mode/walk-mode.gd
+++ addons/walk-mode/walk-mode.gd
@@ -50,13 +50,8 @@ func _forward_3d_gui_input(_camera: Camera3D, event: InputEvent):
 			if(character_spawned == false):
 				return
 			
-			var walk_camera = character.find_child("Camera", true, false)
-			if walk_camera:
-				var cam_transform : Transform3D = walk_camera.global_transform
-				var forward_direction = cam_transform.basis.z.normalized()
-				cam_transform.origin -= forward_direction * 2.5
-				_camera.global_transform = cam_transform
-			
+			var walk_camera : Camera3D = character.find_child("Camera", true, false)
+			_camera.global_transform  = walk_camera.global_transform		
 			canvas_viewport.gui_disable_input = true
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 			get_tree().edited_scene_root.remove_child(character)
