95c667108fc61ecf214a667f3542862fe03337e4
Robin Cooper <robin.cooper@autStand.com>
2025-04-21 11:42:05 -0400

    Add `size` property to BeltConveyor and use it to adjust conveyor instead of `scale`

--- src/Conveyor/belt_conveyor.gd
+++ src/Conveyor/belt_conveyor.gd
@@ -1,6 +1,6 @@
 @tool
 class_name BeltConveyor
-extends Node3D
+extends ResizableNode3D
 
 enum ConvTexture {
 	STANDARD,
@@ -9,29 +9,85 @@ enum ConvTexture {
 
 signal speed_changed
 
+static func _get_constrained_size(new_size: Vector3) -> Vector3:
+	# Don't allow belt conveyors to be shorter than the total length of their ends.
+	# Ends' length varies with height.
+	var height := new_size.y
+	var end_length := height / 2.0
+	# For sanity, ensure that the middle's length is non-zero.
+	var middle_min_length := 0.01
+	var minimum_length := end_length * 2.0 + middle_min_length
+	new_size.x = max(new_size.x, minimum_length)
+	return new_size
+
+func _on_size_changed() -> void:
+	var length := size.x
+	var height := size.y
+	var width := size.z
+
+	# Get components that need to be adjusted.
+	var end1 := ce1
+	var end2 := ce2
+	var middle_body := sb
+	var middle_mesh := mesh
+	var middle_collision_shape := get_node_and_resource("StaticBody3D/CollisionShape3D:shape")[1] as BoxShape3D
+	if not (is_instance_valid(end1)
+			and is_instance_valid(end2)
+			and is_instance_valid(middle_body)
+			and is_instance_valid(middle_mesh)
+			and is_instance_valid(middle_collision_shape)):
+		# Children not instantiated yet.
+		# Do nothing and wait to get called again later.
+		return
+
+	# Calculate dimensions of the components (when they don't match the ones above).
+	# Ends' length varies with height.
+	var end_length := height / 2.0
+	# Middle length fills the rest.
+	var middle_length := length - 2.0 * end_length
+
+	# Update component sizes.
+	var middle_size := Vector3(middle_length, height, width)
+	var end_size := Vector3(end_length, height, width)
+	# Size of the mesh at scale=1. (Size per scale unit.)
+	var middle_mesh_base_size := Vector3(1, 0.5, 2)
+	middle_mesh.scale = middle_size / middle_mesh_base_size
+	middle_collision_shape.size = middle_size
+	end1.size = end_size
+	end2.size = end_size
+
+	# Update materials.
+	_update_belt_material_scale()
+	_update_metal_material_scale()
+
+	# Update component positions.
+	# Ensures that the top surface of the conveyor is on the y=0 plane.
+	var base_pos = Vector3(0, -height / 2.0, 0)
+	middle_body.position = base_pos
+	var end_offset_x = length / 2.0 - end_length
+	end1.position = Vector3(base_pos.x + end_offset_x, base_pos.y, base_pos.z)
+	end2.position = Vector3(base_pos.x + -end_offset_x, base_pos.y, base_pos.z)
+
+## Convert existing scale into size.
+## Avoids doing anything if size has already been set to a non-default value.
+func migrate_scale_to_size():
+	if scale == Vector3.ONE:
+		return  # scale already reset; nothing to do
+	if size != SIZE_DEFAULT:
+		return  # size isn't default; assume migration has already happened despite the unexpected scale.
+	var scale_original = scale
+	scale = Vector3.ONE
+	size = scale_original * Vector3(1, 0.5, 1) + Vector3(0.5, 0, 0)
+
 @export var BeltColor : Color = Color(1, 1, 1, 1):
 	set(value):
 		BeltColor = value
-		if belt_material:
-			(belt_material as ShaderMaterial).set_shader_parameter("ColorMix", BeltColor)
-		var ce1 = get_conveyor_end1()
-		var ce2 = get_conveyor_end2()
-		if ce1 and ce1.belt_material:
-			(ce1.belt_material as ShaderMaterial).set_shader_parameter("ColorMix", BeltColor)
-		if ce2 and ce2.belt_material:
-			(ce2.belt_material as ShaderMaterial).set_shader_parameter("ColorMix", BeltColor)
+		_update_material_color()
 
 @export var BeltTexture = ConvTexture.STANDARD:
 	set(value):
 		BeltTexture = value
-		if belt_material:
-			(belt_material as ShaderMaterial).set_shader_parameter("BlackTextureOn", BeltTexture == ConvTexture.STANDARD)
-		var ce1 = get_conveyor_end1()
-		var ce2 = get_conveyor_end2()
-		if ce1 and ce1.belt_material:
-			(ce1.belt_material as ShaderMaterial).set_shader_parameter("BlackTextureOn", BeltTexture == ConvTexture.STANDARD)
-		if ce2 and ce2.belt_material:
-			(ce2.belt_material as ShaderMaterial).set_shader_parameter("BlackTextureOn", BeltTexture == ConvTexture.STANDARD)
+		_update_material_texture()
 
 
 @export var Speed: float = 2:
@@ -39,14 +95,9 @@ signal speed_changed
 		if value == Speed:
 			return
 		Speed = value
+		_update_speed()
+		_update_belt_material_scale()
 		speed_changed.emit()
-		var ce1 = get_conveyor_end1()
-		var ce2 = get_conveyor_end2()
-		if ce1:
-			ce1.Speed = Speed
-		if ce2:
-			ce2.Speed = Speed
-		UpdateBeltMaterialScale()
 		
 		# dont write until the group is initialized
 		if register_speed_tag_ok and speed_tag_group_init:
@@ -73,11 +124,12 @@ signal speed_changed
 			sb_end2.physics_material_override = value
 
 var sb: StaticBody3D
+var ce1: ConveyorEnd
+var ce2: ConveyorEnd
 var mesh: MeshInstance3D
 var belt_material: Material
 var metal_material: Material
 var belt_position: float = 0.0
-var box_size: Vector3
 
 var register_speed_tag_ok := false
 var register_running_tag_ok := false
@@ -117,55 +169,18 @@ func _validate_property(property: Dictionary):
 		property.usage = PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_NO_INSTANCE_STATE if OIPComms.get_enable_comms() else PROPERTY_USAGE_NONE
 	elif property.name == "running_tag_name":
 		property.usage = PROPERTY_USAGE_DEFAULT if OIPComms.get_enable_comms() else PROPERTY_USAGE_NONE
-		
-func get_conveyor_end1():
-	return get_node_or_null("ConveyorEnd")
-func get_conveyor_end2():
-	return get_node_or_null("ConveyorEnd2")
 
 func _init() -> void:
-	set_notify_local_transform(true)
+	SIZE_DEFAULT = Vector3(4, 0.5, 1.524)
 
-func _ready() -> void:
-	sb = get_node("StaticBody3D") as StaticBody3D
-	mesh = get_node("StaticBody3D/MeshInstance3D") as MeshInstance3D
-	mesh.mesh = mesh.mesh.duplicate() as Mesh
-	belt_material = mesh.mesh.surface_get_material(0).duplicate() as Material
-	metal_material = mesh.mesh.surface_get_material(1).duplicate() as Material
-	mesh.mesh.surface_set_material(0, belt_material)
-	mesh.mesh.surface_set_material(1, metal_material)
-	mesh.mesh.surface_set_material(2, metal_material)
-	
-	belt_material.set_shader_parameter("BlackTextureOn", BeltTexture == ConvTexture.STANDARD)
-	var ce1 = get_conveyor_end1()
-	var ce2 = get_conveyor_end2()
-	if ce1 and ce1.belt_material:
-		ce1.belt_material.set_shader_parameter("BlackTextureOn", BeltTexture == ConvTexture.STANDARD)
-	if ce2 and ce2.belt_material:
-		ce2.belt_material.set_shader_parameter("BlackTextureOn", BeltTexture == ConvTexture.STANDARD)
-	
-	belt_material.set_shader_parameter("ColorMix", BeltColor)
-	if ce1 and ce1.belt_material:
-		ce1.belt_material.set_shader_parameter("ColorMix", BeltColor)
-	if ce2 and ce2.belt_material:
-		ce2.belt_material.set_shader_parameter("ColorMix", BeltColor)
-	
-	if ce1:
-		ce1.Speed = Speed
-	if ce2:
-		ce2.Speed = Speed
-	
-	if ce1:
-		var sb1 = ce1.get_node("StaticBody3D") as StaticBody3D
-		if sb1:
-			sb1.physics_material_override = sb.physics_material_override
-	if ce2:
-		var sb2 = ce2.get_node("StaticBody3D") as StaticBody3D
-		if sb2:
-			sb2.physics_material_override = sb.physics_material_override
-			
-	UpdateBeltMaterialScale()
-	UpdateMetalMaterialScale()
+func _on_instantiated() -> void:
+	_setup_references()
+	_setup_materials()
+	_update_material_texture()
+	_update_material_color()
+	_update_speed()
+	_update_physics_material()
+	_on_size_changed()
 
 func _enter_tree() -> void:
 	SimulationEvents.simulation_started.connect(_on_simulation_started)
@@ -174,28 +189,14 @@ func _enter_tree() -> void:
 	OIPComms.tag_group_polled.connect(_tag_group_polled)
 	OIPComms.enable_comms_changed.connect(func() -> void: _enable_comms_changed = OIPComms.get_enable_comms)
 
+func _ready() -> void:
+	migrate_scale_to_size()
+
 func _exit_tree() -> void:
 	SimulationEvents.simulation_started.disconnect(_on_simulation_started)
 	SimulationEvents.simulation_ended.disconnect(_on_simulation_ended)
 	OIPComms.tag_group_initialized.disconnect(_tag_group_initialized)
 	OIPComms.tag_group_polled.disconnect(_tag_group_polled)
-
-func _notification(what: int) -> void:
-	if what == NOTIFICATION_LOCAL_TRANSFORM_CHANGED:
-		if scale.y != 1:
-			scale = Vector3(scale.x, 1, scale.z)
-			return
-		_on_scale_changed()
-
-func _on_scale_changed() -> void:
-	UpdateBeltMaterialScale()
-	UpdateMetalMaterialScale()
-	var ce1 = get_conveyor_end1()
-	var ce2 = get_conveyor_end2()
-	if ce1:
-		ce1.OnOwnerScaleChanged(scale)
-	if ce2:
-		ce2.OnOwnerScaleChanged(scale)
 
 func _physics_process(delta: float) -> void:
 	if SimulationEvents.simulation_running:
@@ -209,13 +210,61 @@ func _physics_process(delta: float) -> void:
 		if belt_position >= 1.0:
 			belt_position = 0.0
 
-func UpdateBeltMaterialScale() -> void:
-	if belt_material and Speed != 0:
-		(belt_material as ShaderMaterial).set_shader_parameter("Scale", scale.x * sign(Speed))
+func _setup_references() -> void:
+	sb = get_node("StaticBody3D") as StaticBody3D
+	ce1 = get_node("ConveyorEnd") as ConveyorEnd
+	ce2 = get_node("ConveyorEnd2") as ConveyorEnd
+	mesh = get_node("StaticBody3D/MeshInstance3D") as MeshInstance3D
+	belt_material = mesh.mesh.surface_get_material(0)
+	metal_material = mesh.mesh.surface_get_material(1)
 
-func UpdateMetalMaterialScale() -> void:
+func _setup_materials() -> void:
+	belt_material = mesh.mesh.surface_get_material(0).duplicate() as Material
+	metal_material = mesh.mesh.surface_get_material(1).duplicate() as Material
+	mesh.set_surface_override_material(0, belt_material)
+	mesh.set_surface_override_material(1, metal_material)
+	mesh.set_surface_override_material(2, metal_material)
+
+func _update_material_texture() -> void:
+	if belt_material:
+		belt_material.set_shader_parameter("BlackTextureOn", BeltTexture == ConvTexture.STANDARD)
+	if ce1 and ce1.belt_material:
+		ce1.belt_material.set_shader_parameter("BlackTextureOn", BeltTexture == ConvTexture.STANDARD)
+	if ce2 and ce2.belt_material:
+		ce2.belt_material.set_shader_parameter("BlackTextureOn", BeltTexture == ConvTexture.STANDARD)
+	
+func _update_material_color() -> void:
+	if belt_material:
+		belt_material.set_shader_parameter("ColorMix", BeltColor)
+	if ce1 and ce1.belt_material:
+		ce1.belt_material.set_shader_parameter("ColorMix", BeltColor)
+	if ce2 and ce2.belt_material:
+		ce2.belt_material.set_shader_parameter("ColorMix", BeltColor)
+	
+func _update_speed() -> void:
+	if ce1:
+		ce1.Speed = Speed
+	if ce2:
+		ce2.Speed = Speed
+	
+func _update_physics_material() -> void:
+	if ce1:
+		var sb1 = ce1.get_node("StaticBody3D") as StaticBody3D
+		if sb1:
+			sb1.physics_material_override = sb.physics_material_override
+	if ce2:
+		var sb2 = ce2.get_node("StaticBody3D") as StaticBody3D
+		if sb2:
+			sb2.physics_material_override = sb.physics_material_override
+
+func _update_belt_material_scale() -> void:
+	if belt_material and Speed != 0:
+		(belt_material as ShaderMaterial).set_shader_parameter("Scale", mesh.scale.x * sign(Speed))
+
+func _update_metal_material_scale() -> void:
 	if metal_material:
-		(metal_material as ShaderMaterial).set_shader_parameter("Scale", scale.x)
+		(metal_material as ShaderMaterial).set_shader_parameter("Scale", mesh.scale.x)
+		(metal_material as ShaderMaterial).set_shader_parameter("Scale2", mesh.scale.y)
 
 func _on_simulation_started() -> void:
 	if enable_comms:
@@ -226,10 +275,6 @@ func _on_simulation_ended() -> void:
 	belt_position = 0.0
 	(belt_material as ShaderMaterial).set_shader_parameter("BeltPosition", belt_position)
 	sb.constant_linear_velocity = Vector3.ZERO
-	for child in sb.get_children():
-		if child is Node3D:
-			child.position = Vector3.ZERO
-			child.rotation = Vector3.ZERO
 
 func _tag_group_initialized(_tag_group_name: String) -> void:
 	if _tag_group_name == speed_tag_group_name:
---
+++ src/ResizableNode3D/resizable_node_3d.gd
@@ -0,0 +1,68 @@
+extends Node3D
+class_name ResizableNode3D
+
+## Minimum allowed value for the size property.
+## The size property will be automatically constrained accordingly.
+var SIZE_MIN = Vector3(0.01, 0.01, 0.01)
+## Default value for the size property.
+var SIZE_DEFAULT = Vector3.ONE
+@export_custom(PROPERTY_HINT_NONE, "suffix:m") var size: Vector3 = Vector3.ONE:
+	set(value):
+		var clamped_size: Vector3 = SIZE_MIN.max(value)
+		var constrained_size = _get_constrained_size(clamped_size)
+		var has_changed = size != constrained_size
+		if has_changed:
+			size = constrained_size
+			_on_size_changed()
+
+
+func _notification(what: int):
+	# Custom notification number.
+	# This would be better implemented in the engine.
+	const NOTIFICATION_OWNER_SCENE_INSTANTIATED = 9999
+	match what:
+		NOTIFICATION_SCENE_INSTANTIATED:
+			_on_scene_instantiated()
+			# Walk the tree to send NOTIFICATION_OWNER_SCENE_INSTANTIATED
+			var nodes_remaining: Array[Node] = get_children(true)
+			while nodes_remaining:
+				var descendent = nodes_remaining.pop_front()
+				nodes_remaining.append_array(descendent.get_children(true))
+				if descendent.owner == self:
+					descendent.notification(NOTIFICATION_OWNER_SCENE_INSTANTIATED)
+		NOTIFICATION_OWNER_SCENE_INSTANTIATED:
+			_on_owner_scene_instantiated()
+
+
+func _on_scene_instantiated() -> void:
+	_on_instantiated()
+
+
+func _on_owner_scene_instantiated() -> void:
+	_on_instantiated()
+
+
+func _on_instantiated() -> void:
+	_on_size_changed()
+
+
+## Override this to constrain size dimensions relative to each other.
+static func _get_constrained_size(new_size: Vector3) -> Vector3:
+	return new_size
+
+
+## Override this to reconfigure nodes when a new value is assigned to the size property.
+func _on_size_changed() -> void:
+	pass
+
+
+## Convert existing scale into size.
+## Avoids doing anything if size has already been set to a non-default value.
+func migrate_scale_to_size():
+	if scale == Vector3.ONE:
+		return  # scale already reset; nothing to do
+	if size != SIZE_DEFAULT:
+		return  # size isn't default; assume migration has already happened despite the unexpected scale.
+	var scale_original = scale
+	scale = Vector3.ONE
+	size = scale_original
