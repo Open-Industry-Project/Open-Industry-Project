583e692049853c56c3f8fedd1b3858038f27debf
Robin Cooper <robin.cooper@autStand.com>
2025-04-21 11:42:05 -0400

    Add `size` property to BeltConveyor and use it to adjust conveyor instead of `scale`

--- src/Conveyor/conveyor_end.gd
+++ src/Conveyor/conveyor_end.gd
@@ -1,54 +1,103 @@
 @tool
-extends Node3D
+class_name ConveyorEnd
+extends ResizableNode3D
 
 @export var Speed: float:
 	set(value):
 		Speed = value
-		UpdateBeltMaterialScale()
-		UpdateBeltMaterialPosition()
+		_update_belt_material_scale()
+		_update_belt_material_position()
 
 var belt_position: float = 0.0
 var running: bool = false
 var static_body: StaticBody3D
 var mesh: MeshInstance3D
 var belt_material: ShaderMaterial
-var belt_shader: Shader
+var metal_material: ShaderMaterial
 
-var prev_scale_x: float = 0.0
+static func _get_constrained_size(new_size: Vector3) -> Vector3:
+	var height := new_size.y
+	new_size.x = height / 2.0
+	return new_size
+
+func _init() -> void:
+	SIZE_MIN = Vector3(0.01, 0.02, 0.01)
+
+func _on_instantiated():
+	_setup_references()
+	_setup_materials()
+	_on_size_changed()
 
 func _ready() -> void:
-	static_body = get_node("StaticBody3D") as StaticBody3D
-	mesh = get_node("MeshInstance3D") as MeshInstance3D
-	mesh.mesh = mesh.mesh.duplicate()
-	belt_material = mesh.mesh.surface_get_material(0).duplicate() as ShaderMaterial
-	mesh.mesh.surface_set_material(0, belt_material)
-	belt_shader = belt_material.shader.duplicate() as Shader
-	belt_material.shader = belt_shader
-
-func OnOwnerScaleChanged(new_owner_scale: Vector3) -> void:
-	if new_owner_scale.x != prev_scale_x:
-		scale = Vector3(1 / new_owner_scale.x, 1, 1)
-		prev_scale_x = new_owner_scale.x
+	pass
 
 func _physics_process(delta: float) -> void:
 	if SimulationEvents.simulation_running:
-		var local_front = global_transform.basis.z.normalized()
 		if not SimulationEvents.simulation_paused:
 			belt_position += Speed * delta
 		if belt_position >= 1.0:
 			belt_position = 0.0
-		var radius: float = 0.25
-		static_body.constant_angular_velocity = local_front * Speed / radius
-		UpdateBeltMaterialPosition()
-	else:
-		belt_position = 0.0
-		static_body.constant_angular_velocity = Vector3.ZERO
-		UpdateBeltMaterialPosition()
+		_update_belt_material_position()
 
-func UpdateBeltMaterialScale() -> void:
+func _on_simulation_started() -> void:
+	_update_belt_velocity()
+
+func _on_simulation_ended() -> void:
+	belt_position = 0.0
+	_update_belt_material_position()
+	_update_belt_velocity()
+
+func _setup_references() -> void:
+	static_body = get_node("StaticBody3D") as StaticBody3D
+	mesh = get_node("MeshInstance3D") as MeshInstance3D
+	belt_material = mesh.mesh.surface_get_material(0) as ShaderMaterial
+	metal_material = mesh.mesh.surface_get_material(1) as ShaderMaterial
+
+func _setup_materials() -> void:
+	belt_material = mesh.mesh.surface_get_material(0).duplicate() as ShaderMaterial
+	metal_material = mesh.mesh.surface_get_material(1).duplicate() as ShaderMaterial
+	mesh.set_surface_override_material(0, belt_material)
+	mesh.set_surface_override_material(1, metal_material)
+
+func _update_belt_material_scale() -> void:
+	const BASE_RADIUS := 0.25
+	const BASE_BELT_LENGTH := PI * BASE_RADIUS
+	var radius: float = size.x
+	var belt_length: float = PI * radius
+	var belt_scale: float = belt_length / BASE_BELT_LENGTH
 	if belt_material && Speed != 0:
-		(belt_material as ShaderMaterial).set_shader_parameter("Scale", sign(Speed))
+		(belt_material as ShaderMaterial).set_shader_parameter("Scale", belt_scale * sign(Speed))
 
-func UpdateBeltMaterialPosition() -> void:
+func _update_metal_material_scale() -> void:
+	if metal_material:
+		(metal_material as ShaderMaterial).set_shader_parameter("Scale", mesh.scale.x)
+		(metal_material as ShaderMaterial).set_shader_parameter("Scale2", mesh.scale.y)
+
+func _update_belt_material_position() -> void:
 	if belt_material:
 		(belt_material as ShaderMaterial).set_shader_parameter("BeltPosition", belt_position * sign(-Speed))
+
+func _on_size_changed():
+	if not (get_node_or_null("MeshInstance3D") and get_node_and_resource("StaticBody3D/CollisionShape3D:shape")[1]):
+		# Children not instantiated yet.
+		# Do nothing and wait to get called again later.
+		return
+	var mesh_base_size := Vector3(0.25, 0.5, 2)
+	$MeshInstance3D.scale = size / mesh_base_size
+	var cylinder := $StaticBody3D/CollisionShape3D.shape as CylinderShape3D
+	cylinder.height = size.z
+	cylinder.radius = size.x
+
+	_update_belt_material_scale()
+	_update_metal_material_scale()
+	_update_belt_velocity()
+
+func _update_belt_velocity():
+	if SimulationEvents.simulation_running:
+		var local_front: Vector3 = global_transform.basis.z.normalized()
+		var radius: float = size.x
+		var new_velocity: Vector3 = local_front * Speed / radius
+		if static_body.constant_angular_velocity != new_velocity:
+			static_body.constant_angular_velocity = new_velocity
+	else:
+		static_body.constant_angular_velocity = Vector3.ZERO
