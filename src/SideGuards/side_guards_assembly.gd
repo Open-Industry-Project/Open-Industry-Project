4f3ebeb57929d177093c18792ab8e53d6439a4b4
Robin Cooper <robin.cooper@autStand.com>
2025-04-25 11:51:50 -0400

    Add ability to have side guard openings in SideGuardsAssembly

--- src/SideGuards/side_guards_assembly.gd
+++ src/SideGuards/side_guards_assembly.gd
@@ -12,10 +12,79 @@ enum Side
 	set(value):
 		right_side = value
 		_update_side(Side.RIGHT, right_side)
+@export var right_side_openings: Array[SideGuardOpening]:
+	set(value):
+		# Unsubscribe from previous openings. They may be replaced with new ones.
+		for opening in right_side_openings:
+			# Shouldn't be null since all nulls should have been replaced by instances.
+			assert(opening != null, "Opening is null.")
+			opening.disconnect("changed", self._on_opening_changed_right)
+
+		# Workaround for faulty duplicate behavior in the editor.
+		# See issue #74918.
+		if right_side_openings.size() == 0:
+			# Assume that we're initializing for the first time if existing Array is empty.
+			for opening in value:
+				# Any openings we see in the new Array possibly came from an original that this instance is a duplicate of.
+				# There's no way to know for sure.
+				# Make all the openings unique to this instance to prevent editing the originals.
+				if opening != null:
+					opening = opening.duplicate(true)
+				right_side_openings.append(opening)
+		else:
+			# If we're not initializing, avoid making unnecessary duplicates.
+			right_side_openings.assign(value)
+
+		# Replace null with a new gap so users don't have to do this by hand.
+		for i in range(right_side_openings.size()):
+			if right_side_openings[i] == null:
+				right_side_openings[i] = SideGuardOpening.new()
+
+		# Subscribe to ensure that side guards update whenever openings change.
+		for opening in right_side_openings:
+			opening.connect("changed", self._on_opening_changed_right)
+
+		# Update side guards to account for added or removed gaps.
+		_on_opening_changed_right()
 @export var left_side = true:
 	set(value):
 		left_side = value
 		_update_side(Side.LEFT, left_side)
+@export var left_side_openings: Array[SideGuardOpening]:
+	set(value):
+		# Unsubscribe from previous openings. They may be replaced with new ones.
+		for opening in left_side_openings:
+			# Shouldn't be null since all nulls should have been replaced by instances.
+			assert(opening != null, "Opening is null.")
+			opening.disconnect("changed", self._on_opening_changed_left)
+
+		# Workaround for faulty duplicate behavior in the editor.
+		# See issue #74918.
+		if left_side_openings.size() == 0:
+			# Assume that we're initializing for the first time if existing Array is empty.
+			for opening in value:
+				# Any openings we see in the new Array possibly came from an original that this instance is a duplicate of.
+				# There's no way to know for sure.
+				# Make all the openings unique to this instance to prevent editing the originals.
+				if opening != null:
+					opening = opening.duplicate(true)
+				left_side_openings.append(opening)
+		else:
+			# If we're not initializing, avoid making unnecessary duplicates.
+			left_side_openings.assign(value)
+
+		# Replace null with a new gap so users don't have to do this by hand.
+		for i in range(left_side_openings.size()):
+			if left_side_openings[i] == null:
+				left_side_openings[i] = SideGuardOpening.new()
+
+		# Subscribe to ensure that side guards update whenever openings change.
+		for opening in left_side_openings:
+			opening.connect("changed", self._on_opening_changed_left)
+
+		# Update side guards to account for added or removed gaps.
+		_on_opening_changed_left()
+
 var conveyor_connected = false;
 
 
@@ -72,8 +141,8 @@ func _update_side(side: SideGuardsAssembly.Side, side_enabled: bool) -> void:
 
 	# Create and arrange side guards
 	var side_extents: Array[float] = _get_side_extents(side)
-	# TODO implement gaps that split the extents into multiple pieces.
 	var side_guard_extents: Array = [side_extents]
+	_insert_openings_into_extents(side_guard_extents, side)
 	_add_or_remove_side_guards(side_node, side_guard_extents.size())
 	_adjust_side_guards(side_node, side_guard_extents, side)
 
@@ -125,6 +194,106 @@ func _get_side_extents(side: SideGuardsAssembly.Side) -> Array[float]:
 	return [-conveyor_length / 2.0, conveyor_length / 2.0]
 
 
+## Cut gaps into the conveyor extents list.
+##
+## Shrink or remove conveyor extents to make room for side guard gaps.
+##
+## @param extent_pairs The list of conveyor extents to cut gaps into.
+func _insert_openings_into_extents(extent_pairs: Array, side: SideGuardsAssembly.Side) -> void:
+	# Sort and merge the gaps for the current side. Zero-size gaps allowed.
+	var side_guards_gaps: Array
+	match side:
+		Side.RIGHT:
+			side_guards_gaps = right_side_openings
+		Side.LEFT:
+			side_guards_gaps = left_side_openings
+	# Gap extents.
+	var gaps: Array = []
+	for gap in side_guards_gaps:
+		var extent_front = gap.position - abs(gap.size) / 2.0
+		var extent_rear = gap.position + abs(gap.size) / 2.0
+		gaps.append([extent_front, extent_rear])
+
+	# Sort gaps by leading edge
+	gaps.sort_custom(func(a, b): return a[0] < b[0])
+
+	# Merge overlapping gaps
+	var merged_gaps: Array = []
+	for gap in gaps:
+		if merged_gaps.is_empty():
+			merged_gaps.append(gap)
+			continue
+
+		var extent_front = gap[0]
+		var extent_rear = gap[1]
+		var last_gap = merged_gaps[-1]
+		var last_extent_front = last_gap[0]
+		var last_extent_rear = last_gap[1]
+
+		if last_extent_rear < extent_front:
+			# No overlap
+			merged_gaps.append(gap)
+		elif last_extent_rear < extent_rear:
+			# Partial overlap
+			merged_gaps[-1] = [last_extent_front, extent_rear] as Array[float]
+		# Otherwise, full overlap; ignore
+
+	# Cut gaps into the given extents
+	# Don't assume any sorting on the extents
+	var i = 0
+	while i < extent_pairs.size():
+		var extent = extent_pairs[i]
+		var extent_front = extent[0]
+		var extent_rear = extent[1]
+
+		if extent_rear < extent_front:
+			# Sort extents
+			extent_front = extent[1]
+			extent_rear = extent[0]
+			extent_pairs[i] = [extent_front, extent_rear] as Array[float]
+
+		# Drop empty extents
+		if extent_front == extent_rear:
+			extent_pairs.remove_at(i)
+			continue
+
+		for gap in merged_gaps:
+			var gap_front = gap[0]
+			var gap_rear = gap[1]
+
+			if extent_rear <= gap_front:
+				# Extent is entirely before the gap
+				continue
+
+			if extent_front < gap_front and gap_rear < extent_rear:
+				# Gap is fully inside the extent; split it
+				extent_pairs.insert(i + 1, [gap_rear, extent_rear])
+				extent_pairs[i] = [extent_front, gap_front] as Array[float]
+				break
+
+			if gap_front < extent_front and extent_rear < gap_rear:
+				# Extent is fully inside the gap; remove it
+				extent_pairs.remove_at(i)
+				i -= 1
+				break
+
+			if extent_front < gap_front and gap_front < extent_rear:
+				# Gap clips the end of the extent
+				extent_pairs[i] = [extent_front, gap_front] as Array[float]
+				break
+
+			if extent_front < gap_rear and gap_rear < extent_rear:
+				# Gap clips the start of the extent
+				extent_pairs[i] = [gap_rear, extent_rear] as Array[float]
+				# More gaps might overlap
+				extent = extent_pairs[i]
+				extent_front = extent[0]
+				extent_rear = extent[1]
+
+		i += 1
+
+
+
 func _add_or_remove_side_guards(side_node: Node3D, desired_side_guard_count: int) -> void:
 	# Add or remove guards
 	# Assume all children are guards.
@@ -164,7 +333,7 @@ func _adjust_side_guards(side_node: Node3D, side_guard_extents: Array, side: Sid
 	const VERTICAL_OFFSET: float = -0.25
 	for i in range(side_node.get_child_count()):
 		var guard: Node3D = side_node.get_child(i)
-		var extent: Array[float] = side_guard_extents[i]
+		var extent: Array = side_guard_extents[i] as Array[float]
 		var ext_start: float = extent[0]
 		var ext_end: float = extent[1]
 		var ext_length = ext_end - ext_start
@@ -190,3 +359,9 @@ func _adjust_side_guards(side_node: Node3D, side_guard_extents: Array, side: Sid
 		var length_adjustment := Transform3D().scaled(Vector3(guard_scale_x, 1, 1))
 		# Apply all transforms.
 		guard.transform = mount_transform * facing_transform * guard_base_transform * length_adjustment
+
+func _on_opening_changed_left() -> void:
+	_update_side(Side.LEFT, left_side)
+
+func _on_opening_changed_right() -> void:
+	_update_side(Side.RIGHT, right_side)
