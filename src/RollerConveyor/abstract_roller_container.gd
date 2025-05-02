e5682a66503cbbc5f390497ee0e3ee05d0169324
Robin Cooper <robin.cooper@autStand.com>
2025-05-02 18:10:42 -0400

    Revise translations of `AbstractRollerContainer`, `RollerConveyorEnd`, and `Rollers` to be closer to their source

--- src/RollerConveyor/abstract_roller_container.gd
+++ src/RollerConveyor/abstract_roller_container.gd
@@ -2,38 +2,87 @@
 extends Node3D
 class_name AbstractRollerContainer
 
+signal width_changed(width: float)
+signal length_changed(length: float)
+signal roller_length_changed(length: float)
+signal roller_rotation_changed(rotation_degrees: Vector3)
 signal roller_added(roller: Roller)
 signal roller_removed(roller: Roller)
 
-signal roller_rotation_changed(rotation: Vector3)
-
+var _width: float = 2
+var _length: float = 1
+var _roller_length: float = 2
 var _roller_skew_angle_degrees: float = 0.0
 
-# Virtual method to be overridden
-func _get_rollers() -> Array[Roller]:
-	var rollers: Array[Roller] = []
-	return rollers
+func _init() -> void:
+	roller_added.connect(self._handle_roller_added)
+	roller_removed.connect(self._handle_roller_removed)
 
+# Virtual method to be overridden
 func setup_existing_rollers() -> void:
 	for roller in _get_rollers():
-		emit_signal("roller_added", roller)
-
-func set_roller_skew_angle(angle_degrees: float) -> void:
-	_roller_skew_angle_degrees = angle_degrees
-	emit_signal("roller_rotation_changed", _get_rotation_from_skew_angle(angle_degrees))
-
-func set_width(width: float) -> void:
-	for roller in _get_rollers():
-		roller.set_length(width)
-
-func set_length(_length: float) -> void:
-	pass
+		# If length_changed has already been fired, then we've already
+		# added and subscribed some Rollers, but we still need to
+		# subscribe the original ones. To ensure that each Roller is
+		# only subscribed once, we're going to unsubscribe them all,
+		# then then subscribe them.
+		roller_removed.emit(roller)
+		roller_added.emit(roller)
 
 func on_owner_scale_changed(scale: Vector3) -> void:
 	rescale_inverse(scale)
 
-func rescale_inverse(parent_scale: Vector3) -> void:
-	scale = Vector3(1.0 / parent_scale.x, 1.0 / parent_scale.y, 1.0 / parent_scale.z)
+func rescale_inverse(owner_scale: Vector3) -> void:
+	scale = Vector3(1.0 / owner_scale.x, 1.0 / owner_scale.y, 1.0 / owner_scale.z)
 
+func set_width(width: float) -> void:
+	var changed: bool = _width != width
+	_width = width
+	if changed:
+		update_roller_length()
+		width_changed.emit(_width)
+
+func set_length(length: float) -> void:
+	var changed: bool = _length != length
+	_length = length
+	if changed:
+		length_changed.emit(_length)
+
+func set_roller_skew_angle(skew_angle_degrees: float) -> void:
+	var changed: bool = fmod(_roller_skew_angle_degrees, 360.0) != fmod(skew_angle_degrees, 360.0)
+	_roller_skew_angle_degrees = skew_angle_degrees
+	if changed:
+		roller_rotation_changed.emit(_get_rotation_from_skew_angle(_roller_skew_angle_degrees))
+		update_roller_length()
+
+func update_roller_length() -> void:
+	if abs(fmod(_roller_skew_angle_degrees, 180.0)) == 90.0:
+		return
+	var new_length: float = _width / cos(_roller_skew_angle_degrees * 2.0 * PI / 360.0)
+	var changed: bool = new_length != _roller_length
+	_roller_length = new_length
+	if changed:
+		roller_length_changed.emit(_roller_length)
+
+# Virtual method to be overridden
+func _get_rollers() -> Array[Roller]:
+	var rollers: Array[Roller] = []
+	for child in get_children():
+		if child is Roller:
+			rollers.append(child)
+	return rollers
+
+func _handle_roller_added(roller: Roller) -> void:
+	roller.set_rotation_degrees(_get_rotation_from_skew_angle(_roller_skew_angle_degrees))
+	roller.set_length(_roller_length)
+
+	roller_rotation_changed.connect(roller.set_rotation_degrees)
+	roller_length_changed.connect(roller.set_length)
+
+func _handle_roller_removed(roller: Roller) -> void:
+	roller_rotation_changed.disconnect(roller.set_rotation_degrees)
+	roller_length_changed.disconnect(roller.set_length)
+
+# Virtual method to be overridden
 func _get_rotation_from_skew_angle(angle_degrees: float) -> Vector3:
 	return Vector3(0, angle_degrees, 0)
--- src/RollerConveyor/rollers.gd
+++ src/RollerConveyor/rollers.gd
@@ -6,22 +6,18 @@ class_name Rollers
 
 const ROLLERS_DISTANCE: float = 0.33
 
+func _init() -> void:
+	length_changed.connect(_add_or_remove_rollers)
+
 func _ready() -> void:
 	fix_rollers()
 
-func _get_rollers() -> Array[Roller]:
-	var rollers: Array[Roller] = []
-	for child in get_children():
-		if child is Roller:
-			rollers.append(child)
-	return rollers
+func _add_or_remove_rollers(conveyor_length: float) -> void:
+	var rounded_length: int = round(conveyor_length / ROLLERS_DISTANCE) + 1
+	var roller_count: int = get_child_count()
+	var desired_roller_count: int = rounded_length - 2
 
-func set_length(conveyor_length: float) -> void:
-	var rounded_length = round(conveyor_length / ROLLERS_DISTANCE) + 1
-	var roller_count = get_child_count()
-	var desired_roller_count = rounded_length - 2
-
-	var difference = desired_roller_count - roller_count
+	var difference: int = desired_roller_count - roller_count
 
 	if difference > 0:
 		for i in difference:
@@ -29,7 +25,7 @@ func set_length(conveyor_length: float) -> void:
 	elif difference < 0:
 		for i in range(1, -difference + 1):
 			var roller = get_child(roller_count - i) as Roller
-			emit_signal("roller_removed", roller)
+			roller_removed.emit(roller)
 			remove_child(roller)
 			roller.queue_free()
 
@@ -38,7 +34,7 @@ func spawn_roller() -> void:
 	add_child(roller, true)
 	roller.owner = self.owner
 	roller.position = Vector3(ROLLERS_DISTANCE * get_child_count(), 0, 0)
-	emit_signal("roller_added", roller)
+	roller_added.emit(roller)
 	fix_rollers()
 
 func fix_rollers() -> void:
