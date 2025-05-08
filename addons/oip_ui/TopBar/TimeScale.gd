f24051ef82154937564bd2422fe1ac06c109605c
gugak <107577102+Salijoghli@users.noreply.github.com>
2025-05-08 00:24:25 -0700

    Fixes to the code.
    Removed set_process(true)
    Listening to ProjectSettings::settings_changed() instead of checking every frame.

--- addons/oip_ui/TopBar/TimeScale.gd
+++ addons/oip_ui/TopBar/TimeScale.gd
@@ -1,7 +1,6 @@
 @tool
 extends Button
 
-var time_scale_options := [1.0, 2.0, 4.0]
 var last_known_scale := 1.0
 
 func _ready():
@@ -9,7 +8,7 @@ func _ready():
 	tooltip_text = "Change speed (1x, 2x, 4x)"
 
 	last_known_scale = ProjectSettings.get_setting("application/run/scene_time_scale", 1.0)
-	if last_known_scale < 0:
+	if last_known_scale <= 0:
 		last_known_scale = 0.1
 		ProjectSettings.set_setting("application/run/scene_time_scale", last_known_scale)
 		ProjectSettings.save()
@@ -18,7 +17,9 @@ func _ready():
 	_update_button_text()
 
 	toggled.connect(_on_toggled)
-	set_process(true)
+
+	# Connect to settings_changed to monitor changes
+	ProjectSettings.settings_changed.connect(_on_settings_changed)
 
 func _on_toggled(_pressed: bool):
 	var new_scale: float
@@ -31,15 +32,14 @@ func _on_toggled(_pressed: bool):
 	else:
 		new_scale = 1.0
 
+	if new_scale <= 0:
+		new_scale = 0.1
+
 	ProjectSettings.set_setting("application/run/scene_time_scale", new_scale)
 	ProjectSettings.save()
-	Engine.time_scale = new_scale
-	last_known_scale = new_scale
-	_update_button_text()
 
-func _process(_delta: float) -> void:
+func _on_settings_changed():
 	var current_scale = ProjectSettings.get_setting("application/run/scene_time_scale", 1.0)
-
 	if current_scale <= 0:
 		current_scale = 0.1
 		ProjectSettings.set_setting("application/run/scene_time_scale", current_scale)
@@ -50,7 +50,6 @@ func _process(_delta: float) -> void:
 		last_known_scale = current_scale
 		Engine.time_scale = current_scale
 		_update_button_text()
-
 
 func _update_button_text():
 	text = str("%.2f" % last_known_scale) + "x"
