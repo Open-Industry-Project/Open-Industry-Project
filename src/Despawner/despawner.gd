5a2b58176936bf2cffe12ec21a932789f1f7bb0e
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2025-05-29 13:19:51 +0400

    Make root node for `Despawner` Node3D to remove scale warning

--- src/Despawner/despawner.gd
+++ src/Despawner/despawner.gd
@@ -1,16 +1,16 @@
 @tool
 class_name Despawner
-extends Area3D
+extends Node3D
 
-func _enter_tree() -> void:
-	body_entered.connect(_body_entered)
+@onready var _area_3d: Area3D = $Area3D
+
+func _ready() -> void:
+	_area_3d.body_entered.connect(_body_entered)
 
 func _exit_tree() -> void:
-	body_entered.disconnect(_body_entered)
+	_area_3d.body_entered.disconnect(_body_entered)
 
 func _body_entered(node: Node) -> void:
 	var _parent = node.get_parent()
 	if _parent.has_method("selected"):
 		_parent.queue_free()
-
-
