50afd20f87018a4ae58778e956750aef3c7b9a21
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2025-06-02 20:44:01 +0400

    Prevent `Despawner` from despawing none-instanced parts, and forward `Area3D` monitoring property

--- src/Despawner/despawner.gd
+++ src/Despawner/despawner.gd
@@ -4,13 +4,22 @@ extends Node3D
 
 @onready var _area_3d: Area3D = $Area3D
 
+@export var monitoring: bool = true:
+	set(value):
+		if _area_3d:
+			_area_3d.monitoring = value
+	get:
+		return _area_3d.monitoring if _area_3d else true
+
 func _ready() -> void:
-	_area_3d.body_entered.connect(_body_entered)
+	if _area_3d:
+		_area_3d.body_entered.connect(_body_entered)
 
 func _exit_tree() -> void:
-	_area_3d.body_entered.disconnect(_body_entered)
+	if _area_3d and is_instance_valid(_area_3d) and _area_3d.body_entered.is_connected(_body_entered):
+		_area_3d.body_entered.disconnect(_body_entered)
 
 func _body_entered(node: Node) -> void:
 	var _parent = node.get_parent()
-	if _parent.has_method("selected"):
+	if _parent.has_method("selected") and _parent.get("instanced"):
 		_parent.queue_free()
