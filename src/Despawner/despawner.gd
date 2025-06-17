ada9c37752e9538be9961e31f6fbe1943492f8a6
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2025-06-17 16:57:50 +0400

    Fix `Despawner` not working when switching scenes

--- src/Despawner/despawner.gd
+++ src/Despawner/despawner.gd
@@ -2,7 +2,7 @@
 class_name Despawner
 extends Node3D
 
-@onready var _area_3d: Area3D = $Area3D
+var _area_3d: Area3D
 
 @export var monitoring: bool = true:
 	set(value):
@@ -12,12 +12,16 @@ extends Node3D
 		return _area_3d.monitoring if _area_3d else true
 
 func _ready() -> void:
-	if _area_3d:
+	_area_3d = get_node("Area3D") as Area3D
+	_area_3d.body_entered.connect(_body_entered)
+
+
+func _enter_tree() -> void:
+	if _area_3d && not _area_3d.body_entered.is_connected(_body_entered):
 		_area_3d.body_entered.connect(_body_entered)
 
 func _exit_tree() -> void:
-	if _area_3d and is_instance_valid(_area_3d) and _area_3d.body_entered.is_connected(_body_entered):
-		_area_3d.body_entered.disconnect(_body_entered)
+	_area_3d.body_entered.disconnect(_body_entered)
 
 func _body_entered(node: Node) -> void:
 	var _parent = node.get_parent()
