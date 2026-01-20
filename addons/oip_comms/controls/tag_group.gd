a917c815c923990a3ead424fbf919d42952b6eeb
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2026-01-19 22:38:35 -0500

    OIPComms: Hide CPU options when selecting modbus_tcp protocol

--- addons/oip_comms/controls/tag_group.gd
+++ addons/oip_comms/controls/tag_group.gd
@@ -62,7 +62,7 @@ func _on_Path_text_changed(_new_text: String) -> void:
 	_on_text_changed(_new_text)
 	
 func update_protocol(_index: int, from_ready := false) -> void:
-	if _index == 2:
+	if _index == 2:  # opc_ua
 		cpu.hide()
 		cpu_label.hide()
 		path_label.text = "Namespace"
@@ -73,7 +73,17 @@ func update_protocol(_index: int, from_ready := false) -> void:
 			gateway.text = "opc.tcp://localhost:4840"
 			if protocol.text == "opc_ua" and not path.text.is_valid_int():
 				path.text = "1"
-	else:
+	elif _index == 1:  # modbus_tcp
+		cpu.hide()
+		cpu_label.hide()
+		path_label.text = "Path"
+		gateway_label.text = "Gateway"
+		
+		# don't overwrite from ready
+		if not from_ready:
+			gateway.text = "localhost"
+			path.text = "1,0"
+	else:  # ab_eip
 		cpu.show()
 		cpu_label.show()
 		path_label.text = "Path"
